1046d55cf742292f58f07a58b725fb33fe7eadc5
Igor <solovyow_iv@nfksber.ru>
2019-05-17 16:25:37 +0300

    раздел все сделки, доработка frontа до соответствия с макетом

--- local/components/nfksber/dogovorview.detail/templates/add/template.php
+++ local/components/nfksber/dogovorview.detail/templates/add/template.php
@@ -1,36 +1,92 @@
-<? //print_r($arResult["LIST_CATEGORY"]) ;?>
-<?// print_r($arResult) ;?>
+<? // print_r($arResult["TEMPLATE_CONTENT_PROPERTY"]) ;?>
+<? //print_r($arResult) ;
+   $Name = $arResult["USER_PROP"]["NAME"];
+   $Surname = $arResult["USER_PROP"]["LAST_NAME"];
+   $Midlenmae = $arResult["USER_PROP"]["SECOND_NAME"];
+   $Phone = $arResult["USER_PROP"]["PERSONAL_PHONE"];
+   $Passport = $arResult["USER_PROP"]["UF_PASSPORT"].' '.$arResult["USER_PROP"]["UF_KEM_VPASSPORT"];
+
+?>
+<script>
+var full_name = {
+    name: '<?=$Name?>',
+    surname: '<?=$Surname?>',
+    midlname:'<?=$Midlenmae?>',
+    phone: '<?=$Phone?>',
+    passport: '<?=$Passport?>'
+}
+</script>
 <h1><?=$arResult["ELEMENT"]["NAME"]?></h1>
- <div class="tender cardDogovor">
+ <div class="tender cardDogovor" style="margin-bottom: 100px;">
     <div class="row">
-        <div class="col-lg-4 col-md-4 col-sm-4">
-            <div class="cardDogovor-boxTool">
-            <div class="select-dogovotTemplate">
-                <label for="select-dogovotTemplate" class="col-form-label">Выбор шаблона договора</label>                
-                <select class="form-control" id="select-dogovotTemplate">
-                    <? foreach($arResult["LIST_CATEGORY"] as $items_category) { ?>
-                    <option value="<?=$items_category["ID"]?>"><?=$items_category["NAME"]?></option>
-                    <? } ?>
-                </select>
-            </div>
-            <div class="select-dogovotTemplate">
-                <label for="select-dogovotTemplate" class="col-form-label">Выбор подкатегории договора</label>
-                <select class="form-control" id="select-dogovotTemplate">                    
-                    <option value="">Договор Купли-Продажи Автомобиля</option>
-                    <option value="">Договор Купли-Продажи Нежилого помещения</option>
-                    <option value="">Договор Купли-Продажи Земельного участка</option>
-                    <option value="">Договор Купли-Продажи Гаража</option>
-                </select>
-            </div>
-                <button class="btn btn-nfk">Подписать договор</button>
-                <button class="btn btn-nfk">Предложить свою редакцию</button>
+        <div class="col-lg-3 col-md-3 col-sm-12">
+            <div class="cardDogovor-boxTool cardPact">
+                <h5>Создать договор из наших шаблонов</h3>
+                <p>В правой части выберите тему договора:</p>
+                <button class="btn btn-nfk" id="template_con">Загрузить договор из шаблона</button>                
+                <?if(!empty($arResult["TEMPLATE_CONTENT"])){ ?>                    
+                    <div class="steps">
+                        <?if(!empty($arResult["TEMPLATE_CONTENT_PROPERTY"])){?>
+                        <div class="t" id="step0"><div class="number">1</div>
+                            <div id="head_c">
+                                <select class="form-control form-control-lg" id="select_type_user">
+                                    <?
+                                        $type_user = explode(",", $arResult["TEMPLATE_CONTENT_PROPERTY"][0]);
+                                    ?>
+                                    <option value="seller">Я <?=$type_user[0]?></option>
+                                    <option value="customer">Я <?=$type_user[1]?></option>
+                                </select>
+                            </div>
+                        </div>
+                        <?     
+                            $arrCount = count($arResult["TEMPLATE_CONTENT_PROPERTY"]);?>
+                            <?for($x=1; $x<$arrCount; $x++){?>
+                                <div class="t" id="step<?=$x?>"><div class="number"><?=$x+1?></div><?=$arResult["TEMPLATE_CONTENT_PROPERTY"][$x]?></div>
+                            <?}?>
+                        <?}?>
+                    </div>
+                <?}?>
+                <h5>Загрузить договор из вашего файла</h3>
+                <p>поддерживаемые форматы(docx, rtf, txt)</p>
+                <form enctype="multipart/form-data" method="post" name="loadcontract"> 
+                    <label for="uploadbtn" class="btn btn-nfk" id="">Выберите собственный файл</label>
+                    <input id="uploadbtn" type="file" name="file"  accept="docx/*" capture="camera" multiple="" required value="Сделать фото" style="display:none;"/>
+                    <label for="load-contract" class="btn btn-nfk" id="btn-load">Загрузить в редактор</label>
+                    <input type="submit" value="Обработать" id="load-contract" style="display:none;"/>
+                </form>
+                <h5>Создать договор используя наш редактор</h5>
+                <p>Нажмите на кнопку "Создать договор в редакторе" и в поле справа наберайте текст используя инструменты для форматирования.</p>
+                <button class="btn btn-nfk" id="create_con">Создать договор в редакторе</button>
             </div>
         </div>               
-        <div class="col-lg-8 col-md-8 col-sm-8"> 
-            <div class="cardDogovor-boxViewText">
-                <div class="dogovor-title">
-                    <h4>Договор</h4>
-                <div>
+        <div class="col-lg-9 col-md-9 col-sm-12">
+        <div class="tools_redactor">
+        <button type="button" class="btn btn-nfk btn-default" data-toggle="tooltip" data-placement="left" title="Сохранить" id="save_btn"><span class="glyphicon glyphicon-floppy-disk"></span></button>
+                <!--<button type="button" class="btn btn-nfk btn-default" data-toggle="tooltip" data-placement="left" title="Сохранить"><span class="glyphicon glyphicon-print"></span></button>-->
+                <button type="button" class="btn btn-nfk btn-default space_right" id="btn-edit" data-toggle="tooltip" data-placement="left" title="Включить редактирование текста" contenteditable="false"><span class="glyphicon glyphicon-pencil"></span></button>                
+                <button type="button" class="btn btn-nfk btn-default" data-toggle="tooltip" data-placement="left" title="Вставить изображение"><span class="glyphicon glyphicon-picture"></span></button>
+                <button type="button" class="btn btn-nfk btn-default form_text"  id="btn-noedit" data-toggle="tooltip" data-placement="left" title="Запретить редактирование выделенного текста"><span class="glyphicon glyphicon-ban-circle"></span></button>
+                <button type="button" class="btn btn-nfk btn-default space_right" id="btn-data" data-toggle="tooltip" data-placement="left" title="Вставить подстановку текущей даты"><span class="glyphicon glyphicon-calendar"></span></button>                
+                <button type="button" class="btn btn-nfk btn-default form_text" id="btn-weight" data-toggle="tooltip" data-placement="left" title="Жирный текст" contenteditable="false"><span class="glyphicon glyphicon-bold"></span></button>
+                <button type="button" class="btn btn-nfk btn-default form_text space_right" id="btn-italic" data-toggle="tooltip" data-placement="left" title="Курсив" contenteditable="false"><span class="glyphicon glyphicon-italic"></span></button>
+                <button type="button" class="btn btn-nfk btn-default form_text" id="btn-question" data-toggle="tooltip" data-placement="left" title="Информация по инструментам" contenteditable="false"><span class="glyphicon glyphicon-question-sign"></span></button>
+        </div>
+        <div class="cardDogovor-boxViewText" id="canvas" contenteditable="false">
+
+            <?if(!empty($arResult["TEMPLATE_CONTENT"])){ ?>                
+                <? echo $arResult["TEMPLATE_CONTENT"]["DETAIL_TEXT"] ;
+            }else {?>            
+                <h3>Вы можете загрузить договор из шаблона или из вашего файла</h3>
+                    <?foreach ($arResult["THREE_TEMPLATE"] as $Item_three){?>
+                        <?if($Item_three["DEPTH_LEVEL"]==1){?>
+                            <span data-id="<?=$Item_three["ID"]?>"><img src="<?=SITE_TEMPLATE_PATH?>/img/folder_contract.png" /><?=$Item_three["NAME"]?></span>
+                        <?}elseif ($Item_three["DEPTH_LEVEL"]==2){?>
+                            <span data-id="<?=$Item_three["ID"]?>" style="padding-left: 35px;"><img src="<?=SITE_TEMPLATE_PATH?>/img/folder_contract.png" /><?=$Item_three["NAME"]?></span>
+                        <?}elseif ($Item_three["DEPTH_LEVEL"]==3){?>
+                            <span data-id="<?=$Item_three["ID"]?>" style="padding-left: 70px;"><img src="<?=SITE_TEMPLATE_PATH?>/img/folder_contract.png" /><?=$Item_three["NAME"]?></span>
+                        <?}?>
+                    <?}?>
+                <?}?>
             </div>
         </div>            
     </div>
--- local/components/nfksber/userpacts.list/templates/.default/template.php
+++ local/components/nfksber/userpacts.list/templates/.default/template.php
@@ -9,7 +9,7 @@
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">Наименование</th>
+      <th scope="col" colspan=2>Наименование</th>
       <th scope="col">Дата</th>
       <th scope="col">Статус</th>
       <th scope="col"></th>
@@ -20,7 +20,14 @@
         foreach($arResult["INFOBLOCK_LIST"]["ARR_SDELKI"] as $pact){
             ?>
             <tr>
-                <th scope="row"><?=$pact["NAME"]?></th>
+                <td scope="row" style="width: 75px;">
+                  <div class="avatar_pact">
+
+                  </div>
+                </td>
+                <td>
+                  <?=$pact["NAME"]?>
+                </td>
                 <td><?=$pact["CREATED_DATE"]?></td>
                 <td><?=$pact["PROPERTIES"]["PACT_STATUS"]["VALUE_XML_ID"]?></td>
                 <td><a href="/my_pacts/edit_my_pact/?ELEMENT_ID=<?=$pact['ID']?>&ACTION=EDIT" target="_blank">Посмотреть</a></td>
@@ -35,7 +42,7 @@
 <?}?>
 <div style="width: 100%; height: 100px;">
 </div>
-<h2>Мои подписанные договора</h2>
+<h2 class="title_line_button">Мои подписанные договора</h2>
 <table class="table">
   <thead>
     <tr>
@@ -50,7 +57,7 @@
         foreach($arResult["SEND_CONTRACT"]as $pact){          
             ?>
             <tr>
-                <th scope="row"><?=$pact["NAME_CONTRACT"]["NAME"]?></th>
+                <td scope="row"><?=$pact["NAME_CONTRACT"]["NAME"]?></td>
                 <td><?=$pact['UF_TIME_SEND_USER_A']->toString();  ?></td>                
                 <!--<td><a href="/upload/private/userfiles/<?=$pact["UF_ID_GROUP"]?>/<?=$pact["UF_ID_USER_GROUP"]?>/pact/<?=$pact["ID"]?>/pact/dog_21_01_2019.pdf?" target="_blank">Посмотреть</a></td>-->
                 <td><img src="<?=SITE_TEMPLATE_PATH?>/img/<?=$pact["STATUS_ICON"]?>"></td>
@@ -77,7 +84,7 @@
         foreach($arResult["MESSAGE_USER"] as $message){
             ?>
             <tr>
-                <th scope="row"><?=$message["UF_TEXT_MESSAGE_USER"]?></th>
+                <td scope="row"><?=$message["UF_TEXT_MESSAGE_USER"]?></td>
                 <td><?=$message["UF_TIME_CREATE_MSG"]->toString()?></td>                
                 <td><a href="/my_pacts/view_my_pact/?id=<?=$pact["ID"]?>" target="_blank">Посмотреть</a></td>
             </tr>   
