c74c9c24f7bbb6aa7d6404559d90b7d72e767f1f
Igor <solovyow_iv@nfksber.ru>
2019-05-20 14:22:45 +0300

    ошибка авторизации

--- local/templates/anypact/components/bitrix/system.auth.form/anypact_popup/template.php
+++ local/templates/anypact/components/bitrix/system.auth.form/anypact_popup/template.php
@@ -4,9 +4,16 @@ CJSCore::Init();
 <div class="regpopup_content_auform">
     <?if($USER->IsAuthorized()):?>
         <p><?echo GetMessage("MAIN_REGISTER_AUTH")?></p>
-        <input type="hidden" name="logout" value="yes" />
+        <input type="hidden" name="logout" value="yes" />        
 		<input type="submit" class="regpopup_content_form_submit" name="logout_butt" value="<?=GetMessage("AUTH_LOGOUT_BUTTON")?>" />
-	<?else:?>
+    <?else:?>
+        <? if ($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR']){            
+            $err_aut_user_type = $arResult['ERROR_MESSAGE']['ERROR_TYPE'];
+            $err_aut_user_mess = $arResult['ERROR_MESSAGE']['MESSAGE'] ;
+            //print_r($arResult['ERROR_MESSAGE']);
+            }
+         ?>
+        <input type="hidden" name="logout" value="<?=$err_aut_user?> " />
         <form name="system_auth_form<?=$arResult["RND"]?>" method="post" target="_top" action="<?=$arResult["AUTH_URL"]?>">
             <?if($arResult["BACKURL"] <> ''):?>
                 <input type="hidden" name="backurl" value="<?=$arResult["BACKURL"]?>" />
@@ -17,7 +24,7 @@ CJSCore::Init();
                 <input type="hidden" name="AUTH_FORM" value="Y" />
                 <input type="hidden" name="TYPE" value="AUTH" />
                 <!--Логин-->
-                <input type="text" name="USER_LOGIN" class="regpopup_content_form_input"  value="" placeholder="<?=GetMessage('AUTH_LOGIN')?>" />
+                <input type="text" name="USER_LOGIN_ERROR" class="regpopup_content_form_input" data-mess="" value="" placeholder="<?=GetMessage('AUTH_LOGIN')?>" />
                         <script>
                             BX.ready(function() {
                                 var loginCookie = BX.getCookie("<?=CUtil::JSEscape($arResult["~LOGIN_COOKIE_NAME"])?>");
@@ -30,7 +37,11 @@ CJSCore::Init();
                             });
                         </script>
                 <!--Пароль-->
-                <input type="password" name="USER_PASSWORD" class="regpopup_content_form_input"  autocomplete="off" />        
+                <input type="password" name="USER_PASSWORD" class="regpopup_content_form_input"  autocomplete="off" />
+                <?
+                if ($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR'])
+                    ShowMessage($arResult['ERROR_MESSAGE']);    
+                ?>     
             <?if ($arResult["STORE_PASSWORD"] == "Y"):?>
                     <input type="checkbox" id="USER_REMEMBER_frm" name="USER_REMEMBER" value="Y" /></td>
                     <label for="USER_REMEMBER_frm" title="<?=GetMessage("AUTH_REMEMBER_ME")?>"><?echo GetMessage("AUTH_REMEMBER_SHORT")?></label>
