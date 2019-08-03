e775c53c015695aabdef36cf7bf1a45fb8a08f45
Трофимов Александр <trofimov.aleksandr@4px.ru>
2019-08-03 13:34:32 +0300

    Договор PDF 2

--- local/components/nfksber/sendcontract.view/templates/.default/script.js
+++ local/components/nfksber/sendcontract.view/templates/.default/script.js
@@ -9,37 +9,6 @@ $(document).ready(function(){
         }
 
     };
-
-    //генерация и скачивани договора в PDF
-    $(document).on('click touchstart', '#download_pdf', function() {
-        let canvas_contr = $('.view-pdf');
-        let canvas_contr_context = String(canvas_contr.html());
-        let id = $(this).attr('data-id');
-        // загружаем содержимое категории
-        $.post(
-            "/response/ajax/get_pdf_dogovor.php", {
-                contect: canvas_contr_context
-            },
-            onAjaxSuccess
-        );
-
-        function onAjaxSuccess(data) {
-            console.log(data);
-            // Здесь мы получаем данные, отправленные сервером и выводим их на экран.
-            /*let result = JSON.parse(data);
-            if(result['TYPE']=='ERROR'){
-                console.log($result['VALUE']);
-                alert(result['VALUE']);
-            }
-            if(result['TYPE']=='SUCCESS'){
-                console.log(result['VALUE']);
-                //alert(result['VALUE']);
-                window.location.href = "/my_pacts/edit_my_pact/?ELEMENT_ID="+result['ID']+"&ACTION=EDIT";
-            }*/
-
-        }
-
-    });
 });
 
 
--- my_pacts/send_contract/contract_pdf.php
+++ my_pacts/send_contract/contract_pdf.php
@@ -1,18 +1,40 @@
-<?require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+<?require_once $_SERVER['DOCUMENT_ROOT'].'/local/php_interface/libraries/libPDFgen/tcpdf.php';
+require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
 
-$html = $_POST['contect'];
+use Bitrix\Main\Loader;
+use Bitrix\Highloadblock as HL;
+use Bitrix\Main\Entity;
+
+Loader::includeModule("highloadblock");
+
+$id = intval($_GET['ID']);
+$hlbl = 7;
+$hlblock = HL\HighloadBlockTable::getById($hlbl)->fetch();
+
+$entity = HL\HighloadBlockTable::compileEntity($hlblock);
+$entity_data_class = $entity->getDataClass();
+
+$rsData = $entity_data_class::getList(array(
+    "select" => array("*"),
+    "order" => array("ID" => "ASC"),
+    "filter" => array("UF_ID_SEND_ITEM"=>$id)
+));
+
+if($obj = $rsData->Fetch()) $arContract = $obj;
+
+$html .= $arContract['UF_TEXT_CONTRACT'];
 
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
+/*$pdf->SetAuthor('Nicola Asuni');
 $pdf->SetTitle('TCPDF Example 021');
 $pdf->SetSubject('TCPDF Tutorial');
-$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');*/
 
 // set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 021', PDF_HEADER_STRING);
+//$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, 'Договор', PDF_HEADER_STRING);
 
 // set header and footer fonts
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
@@ -41,7 +63,7 @@ if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
 // ---------------------------------------------------------
 
 // set font
-$pdf->SetFont('helvetica', '', 9);
+$pdf->SetFont('dejavusans', '', 10);
 
 // add a page
 $pdf->AddPage();
@@ -56,3 +78,4 @@ $pdf->lastPage();
 
 //Close and output PDF document
 $pdf->Output('dogovor.pdf', 'I');
+exit();
