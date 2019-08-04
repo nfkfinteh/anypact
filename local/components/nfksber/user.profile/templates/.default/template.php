0a361ca5582f3fa5bdc3f5c84c498cbdad656891
Трофимов Александр <trofimov.aleksandr@4px.ru>
2019-08-04 22:57:20 +0300

    Интерированя верстка для страницы пользователя

--- local/components/nfksber/user.profile/templates/.default/template.php
+++ local/components/nfksber/user.profile/templates/.default/template.php
@@ -1,177 +1,114 @@
-    <div>
+<div>
+    <?if(empty($arResult['ERROR'])):?>
         <div class="row pt-2">
             <div class="col-lg-3 col-md-4 col-sm-12">
                 <div class="user-avatar">
-                    <span class="user-first-letter">А</span>
-                    <img src="<?=CFile::GetPath($arResult['USER']['PERSONAL_PHOTO'])?>">
+                    <span class="user-first-letter"><?=$arResult['USER']['IN_NAME']?></span>
+                    <img src="<?=$arResult['USER']['IMG_URL']?>">
                     <!--<img src="image/sample_face_150x150.png" alt="Васильев Александр Евгеньевич"> !--164px x 164px-->
                 </div>
                 <h3 class="font-weight-bold mt-4"><?=$arResult['USER']['LAST_NAME']?> <?=$arResult['USER']['NAME']?> <?=$arResult['USER']['SECOND_NAME']?></h3>
-                <a href="#" class="btn btn-nfk btn-uprofile">Оставить отзыв</a>
-                <a href="#" class="btn btn-nfk btn-uprofile">Отправить сообщение</a>
+                <a href="#" class="btn btn-nfk btn-uprofile" data-toggle="modal" data-target=".bd-comment-modal-sm">Оставить отзыв</a>
+                <a href="#" class="btn btn-nfk btn-uprofile" data-toggle="modal" data-target=".bd-message-modal-sm">Отправить сообщение</a>
                 <span class="d-block mt-4">Частное лицо</span>
-                <span class="d-block mt-3">Чебоксары, Ленинский р-н</span>
-                <span class="d-block font-weight-bold mt-4">Подтвержденная регистрация</span>
-                <span class="d-block registration-checked mt-2"><img src="https://gu-st.ru/st/img/logo_nobeta.0a1f5dfe6b.svg" style="width:50%;"/></span>
+                <span class="d-block mt-3"><?=$arResult['USER']['PERSONAL_CITY']?>, <?=$arResult['USER']['PERSONAL_STREET']?></span>
+                <?if($arResult['USER']['UF_ESIA_AUT']==1):?>
+                    <span class="d-block font-weight-bold mt-4">Подтвержденная регистрация</span>
+                    <span class="d-block registration-checked mt-2"><img src="https://gu-st.ru/st/img/logo_nobeta.0a1f5dfe6b.svg" style="width:50%;"/></span>
+                <?endif?>
             </div>
             <div class="col-lg-9 col-md-8 col-sm-12 tenders-list pt-2">
-                <button class="btn-category active">Активные <span class="text-gray">7</span></button>
-                <button class="btn-category">Завершенные <span class="text-black-50">15</span></button>
+                <button class="btn-category <?if($arResult['CURRENT_STATE']=='Y'):?>active<?endif?>" data-state="Y" data-user="<?=$arResult['USER']['ID']?>">Активные <span class="text-gray"><?=$arResult['ACTIVE_ITEMS']?></span></button>
+                <button class="btn-category <?if($arResult['CURRENT_STATE']=='N'):?>active<?endif?>" data-state="N" data-user="<?=$arResult['USER']['ID']?>">Завершенные <span class="text-black-50"><?=$arResult['COMPLETED_ITEMS']?></span></button>
                 <div class="row mt-4">
-                    <div class="col-lg-4 col-md-6 col-sm-12 tender-block">
-                        <div class="tender-post">
-                            <a href="#">
-                                <div class="tender-img">
-                                    <img src="image/tender.png" alt="">
-                                    <span>10.09.2018</span>
+                    <?foreach ($arResult['ITEMS'] as $item):?>
+                        <div class="col-lg-4 col-md-6 col-sm-12 tender-block">
+                            <div class="tender-post">
+                                <a href="/pacts/view_pact/?ELEMENT_ID=<?=$item['ID']?>">
+                                    <div class="tender-img">
+                                        <?if(!empty($item['PREVIEW_PICTURE'])):?>
+                                            <img src="<?=CFile::GetPath($item['PREVIEW_PICTURE'])?>">
+                                        <?else:?>
+                                            <img src="<?=CFile::GetPath($item['DETAIL_PICTURE'])?>">
+                                        <?endif?>
+                                        <span><?=$item['CREATED_DATE']?></span>
+                                    </div>
+                                </a>
+                                <div class="tender-text">
+                                    <a href="/pacts/view_pact/?ELEMENT_ID=<?=$item['ID']?>">
+                                        <h3><?=$item['NAME']?></h3>
+                                        <?if(!empty($item['PREVIEW_TEXT'])):?>
+                                            <p><?=TruncateText($item['PREVIEW_TEXT'], 150)?></p>
+                                        <?else:?>
+                                            <p><?=TruncateText($item['DETAIL_TEXT'], 150)?></p>
+                                        <?endif?>
+
+                                        <span class="tender-price"><?=$item['SUMM_PACT']['VALUE'].' руб.'?></span>
+                                    </a>
                                 </div>
-                            </a>
-                            <div class="tender-text">
-                                <a href="#">
-                                    <h3>Займ</h3>
-                                    <p>Заем краткосрочный, нецелевой, без обеспечения и поручительства</p>
-                                    <span class="tender-price">до 8 000 000 руб.</span>
-                                </a>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-12 tender-block">
-                        <div class="tender-post">
-                            <a href="#">
-                                <div class="tender-img">
-                                    <img src="image/tender.png" alt="">
-                                    <span>10.09.2018</span>
-                                </div>
-                            </a>
-                            <div class="tender-text">
-                                <a href="#">
-                                    <h3>Продажа автомобиля</h3>
-                                    <p>Lada Vesta Год выпуска 2017 Пробег 123 500 км</p>
-                                    <span class="tender-price">до 8 000 000 руб.</span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="tender-indent"></div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-12 tender-block">
-                        <div class="tender-post">
-                            <div class="tender-img">
-                                <a href="#">
-                                    <img src="image/tender.png" alt="">
-                                    <span>10.09.2018</span>
-                                </a>
-                            </div>
-                            <div class="tender-text">
-                                <a href="#">
-                                    <h3>Продажа автомобиля</h3>
-                                    <p>Заем краткосрочный, нецелевой, без обеспечения и поручительстваЗаем краткосрочный, нецелевой, без обеспечения и поручительства</p>
-                                    <span class="tender-price">до 8 000 000 руб.</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-12 tender-block">
-                        <div class="tender-post">
-                            <div class="tender-img">
-                                <a href="#">
-                                    <img src="image/tender.png" alt="">
-                                    <span>10.09.2018</span>
-                                </a>
-                            </div>
-                            <div class="tender-text">
-                                <a href="#">
-                                    <h3>Займ</h3>
-                                    <p>Заем краткосрочный, нецелевой, без обеспечения и поручительства</p>
-                                    <span class="tender-price">до 8 000 000 руб.</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-12 tender-block">
-                        <div class="tender-post">
-                            <a href="#">
-                                <div class="tender-img">
-                                    <img src="image/tender.png" alt="">
-                                    <span>10.09.2018</span>
-                                </div>
-                            </a>
-                            <div class="tender-text">
-                                <a href="#">
-                                    <h3>Займ</h3>
-                                    <p>Заем краткосрочный, нецелевой, без обеспечения и поручительства</p>
-                                    <span class="tender-price">до 8 000 000 руб.</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-12 tender-block">
-                        <div class="tender-post">
-                            <a href="#">
-                                <div class="tender-img">
-                                    <img src="image/tender.png" alt="">
-                                    <span>10.09.2018</span>
-                                </div>
-                            </a>
-                            <div class="tender-text">
-                                <a href="#">
-                                    <h3>Продажа автомобиля</h3>
-                                    <p>Lada Vesta Год выпуска 2017 Пробег 123 500 км</p>
-                                    <span class="tender-price">до 8 000 000 руб.</span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="tender-indent"></div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-12 tender-block">
-                        <div class="tender-post">
-                            <div class="tender-img">
-                                <a href="#">
-                                    <img src="image/tender.png" alt="">
-                                    <span>10.09.2018</span>
-                                </a>
-                            </div>
-                            <div class="tender-text">
-                                <a href="#">
-                                    <h3>Продажа автомобиля</h3>
-                                    <p>Заем краткосрочный, нецелевой, без обеспечения и поручительстваЗаем краткосрочный, нецелевой, без обеспечения и поручительства</p>
-                                    <span class="tender-price">до 8 000 000 руб.</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-12 tender-block">
-                        <div class="tender-post">
-                            <div class="tender-img">
-                                <a href="#">
-                                    <img src="image/tender.png" alt="">
-                                    <span>10.09.2018</span>
-                                </a>
-                            </div>
-                            <div class="tender-text">
-                                <a href="#">
-                                    <h3>Займ</h3>
-                                    <p>Заем краткосрочный, нецелевой, без обеспечения и поручительства</p>
-                                    <span class="tender-price">до 8 000 000 руб.</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+                    <?endforeach?>
                 </div>
             </div>
         </div>
-    </div>
-    <ul class="pagination justify-content-center">
-        <li class="page-item disabled"><a class="page-link" href="#">&larr;</a></li>
-        <li class="page-item disabled"><a class="page-link" href="#">Назад</a></li>
-        <li class="page-item active"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item disabled"><a class="page-link" href="#">.....</a></li>
-        <li class="page-item"><a class="page-link" href="#">5</a></li>
-        <li class="page-item"><a class="page-link" href="#">Вперед</a></li>
-        <li class="page-item"><a class="page-link" href="#">&rarr;</a></li>
-    </ul>
-
+    <?else:?>
+        <div class="row pt-2">
+            <?=$arResult['ERROR']?>
+        </div>
+    <?endif?>
 </div>
+<?=$arResult["NAV_STRING"]?>
+</div>
+
+<div class="modal fade bd-message-modal-sm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="modal-title">Новое сообщение</div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="/response/ajax/add_new_messag_user.php">
+                    <input type="hidden" name="login" value="<?=$arResult['USER']['LOGIN']?>">
+                    <div class="form-group">
+                        <textarea class="form-control " name="message-text"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-nfk d-block cardPact-bBtn submit_message">Отправить</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade bd-comment-modal-sm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="modal-title">Новый отзыв</div>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="/response/ajax/add_new_comment.php">
+                    <input type="hidden" name="login" value="<?=$arResult['USER']['LOGIN']?>">
+                    <div class="form-group">
+                        <textarea class="form-control " name="message-text"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-nfk d-block cardPact-bBtn submit_message">Отправить</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 
 
