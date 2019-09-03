b1e80274bb3ea0eade5b1d9ae050501e40edf2a9
Igor <solovyow_iv@nfksber.ru>
2019-09-03 11:46:36 +0300

    Правильная верстка карточки сделки

--- local/components/nfksber/pactview.detail/templates/.default/template.php
+++ local/components/nfksber/pactview.detail/templates/.default/template.php
@@ -5,75 +5,61 @@
         $disable_a = 'disabled';
     } 
 ?>
-<h1><?=$arResult["ELEMENT"]["NAME"]?></h1>
-======
-<!---------------------------------------------------------------------------------------------------------->
-
-        <div class="row">            
-            <div class="col-md-8 mt-4">
+<h1 class="d-inline-block"><?=$arResult["ELEMENT"]["NAME"]?></h1>    
+    <div class="tender cardPact">
+        <div class="row">
+            <div class="col-md-7 col-lg-8">
                 <? if(!empty($arResult["ELEMENT"]["DETAIL_PICTURE"])){ ?>
-                <div class="cardPact-box">
-                    <?                        
-                            $resize_img = CFile::ResizeImageGet($arResult["ELEMENT"]["DETAIL_PICTURE"], array('width'=>'855', 'height'=>'460'),
-                            BX_RESIZE_IMAGE_EXACT);
+                <div class="slider-sdelka" id="my-slider">
+                    <div class="sp-slides">
+                            <?                        
+                                $resize_img = CFile::ResizeImageGet($arResult["ELEMENT"]["DETAIL_PICTURE"], array('width'=>'855', 'height'=>'460'),
+                                BX_RESIZE_IMAGE_EXACT);
                             ?>
-                            <div class="cardPact-box-BoxMainImg">
-                                <img src="<?=$resize_img["src"]?>" />
-                            </div>                    
-                        <div class="cardPact-box-BoxPrewImg">
                         <?
                             // изображения 
                             $arr_img = $arResult["PROPERTY"]["IMG_FILE"];                    
                             if(!empty($arResult["PROPERTY"]["IMG_FILE"])){
                                 foreach ($arr_img as $url_img){
                                     ?>
-                                    <img src="<?=$url_img["URL"]?>" class="cardPact-box-BoxPrewImg-img"/>
+                                    <div class="sp-slide">
+                                        <img class="sp-image" src="<?=$url_img["URL"]?>">
+                                        <img class="sp-thumbnail" src="<?=$url_img["URL"]?>">
+                                    </div>                                    
                                     <?
                                 }
                             }
                         ?>
-                    </div>                     
+                    </div>
                 </div>
-                <?
-                        }
-                    ?> 
->>>>>>> 5c16d271de55b3f067f49dd8a7bce9e2e3e57d24
-                <h5>Описание</h5>
-                    <?=$arResult["ELEMENT"]["DETAIL_TEXT"]?>
-                <h5>Условия</h5>
-                    <?=$arResult["PROPERTY"]["CONDITIONS_PACT"]["VALUE"]["TEXT"]?>
-                
-=======
-                <?/*<h5 class="mt-5">Комментарии</h5>*/?>
->>>>>>> 5c16d271de55b3f067f49dd8a7bce9e2e3e57d24
+                <? } ?>
             </div>
-            <!--// Левая часть -->
-            <!--Правая часть карточки-->
             <div class="col-md-5 col-lg-4">
-                <span class="cardPact-price"><?=$arResult["PROPERTY"]["SUMM_PACT"]["VALUE"]?> руб.</span>
-                <a href="/pacts/view_pact/view_dogovor/?ELEMENT_ID=<?=$arResult["ELEMENT"]["ID"]?>" class="btn btn-nfk cardPact-bBtn <?=$disable_a?>">Посмотреть договор</a>
+                <span class="cardPact-price">800 000 руб.</span>
+                <a href="#" class="btn btn-nfk cardPact-bBtn">Посмотреть договор</a>
                 <a href="#" class="btn btn-nfk cardPact-bBtn">Посмотреть спецификацию</a>
                 <div class="cardPact-person">
-                    <?if(!empty($arResult["CONTRACT_HOLDER"]["PERSONAL_PHOTO"])):?>
-                        <img src="<?=$arResult["CONTRACT_HOLDER"]["PERSONAL_PHOTO"]?>">
-                    <?else:?>
-                        <div class="cardPact-person__avatar">
-                            <span><?=substr($arResult["CONTRACT_HOLDER"]["NAME"], 0, 1)?></span>
-                        </div>
-                    <?endif?>                    
-                    <span><a href="/profile_user/?ID=<?=$arResult["CONTRACT_HOLDER"]["ID"]?>"><?=$arResult["CONTRACT_HOLDER"]["LAST_NAME"]?> <?=$arResult["CONTRACT_HOLDER"]["NAME"]?></a></span><br>
+                    <img src="image/sample_face_150x150.png">
+                    <span>tap-64</span><br>
+                    <span>Анатолий Степанович</span><br>
                     <span class="text-gray">Неизвестно</span><br>
                 </div>
                 <div class="cardPact-info">
                     <span class="float-left">Репутация</span><span class="float-right cardPact-rating">&#9733; 3,9</span><br>
-                    <span class="float-left">Выполненных сделок</span><span class="float-right "><?=$arResult['DOGOVOR']['CNT']?></span><br>
+                    <span class="float-left">Выполненных сделок</span><span class="float-right ">7</span><br>
                     <span>9 оценок</span>
                 </div>
-                <button type="button" class="btn btn-nfk d-block cardPact-bBtn" data-toggle="modal" data-target=".bd-message-modal-sm">Написать сообщение</button>
+                <button class="btn btn-nfk d-block cardPact-bBtn">Написать сообщение</button>
+            </div>
+            <div class="col-md-8 mt-4">
+                <h5>Описание</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ipsum ipsum, sagittis sollicitudin dignissim et, placerat a arcu. Nunc viverra neque placerat ultrices vehicula. Pellentesque fringilla nibh nec urna pellentesque dignissim.</p>
+                <p>Sed ut feugiat diam. Integer sit amet sollicitudin ipsum. Morbi ut imperdiet mi. Aliquam aliquet lectus sed justo efficitur viverra. Duis ac mauris purus. Nullam quis pulvinar tellus, non hendrerit urna. Curabitur non augue vel velit pulvinar posuere. Morbi quis venenatis lectus. Phasellus lacinia, diam non viverra euismod, nibh dui scelerisque nisi, sed luctus ex sem sit amet ipsum. Maecenas mollis odio quis sem auctor vulputate. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu nisl orci. </p>
+                <h5>Условия</h5>
+                <p class="mb-5">Sed ut feugiat diam. Integer sit amet sollicitudin ipsum. Morbi ut imperdiet mi. Aliquam aliquet lectus sed justo efficitur viverra. Duis ac mauris purus. Nullam quis pulvinar tellus, non hendrerit urna. Curabitur non augue vel velit pulvinar posuere. Morbi quis venenatis lectus. Phasellus lacinia, diam non viverra euismod, nibh dui scelerisque nisi, sed luctus ex sem sit amet ipsum. Maecenas mollis odio quis sem auctor vulputate. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu nisl orci. </p>
+                <h5 class="mt-5">Комментарии</h5>
             </div>
         </div>
-<<<<<<< HEAD
-        <!--Комментарии сделки-->
         <div class="row align-items-center mt-4">
             <div class="col-3 col-sm-2  col-lg-1">
                 <img src="image/sample_face_150x150.png" class="cardPact-comment-avatar" alt="">
@@ -112,34 +98,8 @@
                 <button class="btn btn-nfk cardPact-comment-submit">Комментировать</button>
             </div>
         </div>
-=======
->>>>>>> 5c16d271de55b3f067f49dd8a7bce9e2e3e57d24
-
-        <div class="modal fade bd-message-modal-sm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <div class="modal-title">Новое сообщение</div>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <form id="message_user" action="/response/ajax/add_new_messag_user.php">
-                            <input type="hidden" name="login" value="<?=$arResult['CONTRACT_HOLDER']['LOGIN']?>">
-                            <div class="form-group">
-                                <textarea class="form-control " name="message-text"></textarea>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-nfk d-block cardPact-bBtn submit_message">Отправить</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
+    </div>
+</div>
 
     <script type="text/javascript">
     jQuery( document ).ready(function( $ ) {
