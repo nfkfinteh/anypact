2c17c29cb8b877a2bafe0309362814ef40821cb0
skiminok <skiminok2005@rambler.ru>
2019-11-16 15:28:46 +0300

    fix

---
+++ local/php_interface/unclude/site_closed.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <meta property="og:image" content="/favicon/og-image.jpg">
+    <meta property="og:image:width" content="387">
+    <meta property="og:image:height" content="580">
+    <meta property="og:title" content="AnyPact">
+    <meta property="og:url" content="http://example.com">
+    <meta property="og:description" content="Ваш личный сервис для дистанционного заключения сделок с использованием простой электронной подписи">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:900&amp;subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
+
+    <title>AnyPact.ru - Скоро открытие</title>
+    <style>
+        html, body, .container{
+            font-family: 'Roboto', sans-serif;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        .container{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background: url(upload/image/bg-points.png) calc(24vw - 1088px) calc(34vh - 212px) no-repeat,
+            url(upload/image/bg-points.png) 59vw 50vh no-repeat;
+        }
+        h1{
+            font-size: 72px;
+            text-transform: uppercase;
+            color: #ff6416;
+            font-weight: 900;
+            text-align: center;
+            margin-bottom: 0;
+        }
+        @media screen and (max-width: 420px){
+            h1{
+                font-size: 48px;
+            }
+        }
+    </style>
+</head>
+<body>
+
+<div class="container">
+    <img src="/upload/image/logo-opening-soon.png" alt="AnyPact">
+    <h1>Скоро открытие</h1>
+</div>
+
+</body>
+</html>
