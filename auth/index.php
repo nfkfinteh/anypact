9b2c5dad8f36225527d893a4639db1dac7d48320
Solovyov Igor <skiminok2005@gmail.com>
2019-11-19 18:16:52 +0300

    fix регистрацию  с подтверждением по почте

--- auth/index.php
+++ auth/index.php
@@ -2,12 +2,42 @@
 define("NEED_AUTH", true);
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
-if (is_string($_REQUEST["backurl"]) && strpos($_REQUEST["backurl"], "/") === 0)
-{
-	LocalRedirect($_REQUEST["backurl"]);
-}
+// if (is_string($_REQUEST["backurl"]) && strpos($_REQUEST["backurl"], "/") === 0)
+// {
+// 	LocalRedirect($_REQUEST["backurl"]);
+// }
+
+/* 	получить код и ид пользователя из строки 
+
+	получить код из БД пользователя если совпадают активировать если нет удаляем пользователя, что бы не занимал логин почтой
+	http://anypact.nfksber.ru/auth/index.php?confirm_registration=yes&confirm_user_id=44&confirm_code=RgdZApuR
+*/
 
 $APPLICATION->SetTitle("Авторизация");
+
+$getIDUser 		= $_GET["confirm_user_id"];
+$getUserCode 	= $_GET["confirm_code"];
+
+$rsUser = CUser::GetByID($getIDUser);
+$arUser = $rsUser->Fetch();
+
+if($getUserCode == $arUser["CONFIRM_CODE"]){
+	// активируем пользователя
+	$fields = Array(
+		"ACTIVE"            => "Y"
+		);
+	$user = new CUser;
+	$user->Update($getIDUser, $fields);
+	?><p>Вы зарегистрированы и успешно авторизовались.</p><?
+
+}else {
+	// удаляем пользователя
+
+}
+
+echo "<pre>"; print_r($arUser["CONFIRM_CODE"]); echo "</pre>";
+
+
 ?>
 <p>Вы зарегистрированы и успешно авторизовались.</p>
  
