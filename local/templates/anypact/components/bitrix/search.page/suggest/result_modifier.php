ffc102a8c139e7ee7188f08e100d0c8f1820383c
Трофимов Александр <trofimov.aleksandr@4px.ru>
2019-12-08 14:43:49 +0300

    fix Ошибка поиса людей

--- local/templates/anypact/components/bitrix/search.page/suggest/result_modifier.php
+++ local/templates/anypact/components/bitrix/search.page/suggest/result_modifier.php
@@ -1,5 +1,5 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
+$arResult['USERS'] = [];
 #поиск пользователей
 $arFilter['NAME'] = $arResult['REQUEST']['QUERY'];
 $arFilter['UF_HIDE_PROFILE'] = 0;
