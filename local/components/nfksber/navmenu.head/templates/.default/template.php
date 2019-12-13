6c0ab91ba1d2ae533975760c3a3058323e931e9f
Трофимов Александр <trofimov.aleksandr@4px.ru>
2019-12-13 18:14:48 +0300

    Отображение выбора города на мобильнике

--- local/components/nfksber/navmenu.head/templates/.default/template.php
+++ local/components/nfksber/navmenu.head/templates/.default/template.php
@@ -1,6 +1,9 @@
 <?//print_r($arResult["ARR_ITEM_MENU"]);?>
 
 <ul class="navbar-nav mr-4" style="margin-right: 0 !important;">
+    <li class="mobile-location">
+        <span class="location">Выберите город</span>
+    </li>
 	<?foreach($arResult["ARR_ITEM_MENU"] as $MenuItem) {?>
 		<li class="nav-item">
 			<a class="nav-link <?=$MenuItem["CLASS"]?>" href="<?=$MenuItem["URL"]?>"><?=$MenuItem["NAME"]?></a> 
