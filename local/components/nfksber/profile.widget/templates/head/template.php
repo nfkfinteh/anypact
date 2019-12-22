316897482ba5f7cccac3a8b872e9b35d7545b0d9
Трофимов Александр <trofimov.aleksandr@4px.ru>
2019-12-22 22:42:12 +0300

    Исправлена верстка профиля в шапке

--- local/components/nfksber/profile.widget/templates/head/template.php
+++ local/components/nfksber/profile.widget/templates/head/template.php
@@ -1,19 +1,22 @@
 <div class="widget_user_profile">
-    <? // если есть фото 
-    if($arResult["PERSONAL_PHOTO"] != ""){
-    ?>
     <div class="login-information">
         <div class="login-information-container">
-            <div class="login-information-photo">
-                <img src="<?=$arResult["PERSONAL_PHOTO"]?>">
-                <!-- <a href="#" class="login-information-message-counter">4</a> -->
-            </div>
+            <? if(empty($arResult["PERSONAL_PHOTO"])):?>
+                <div class="widget_user_profile_avatar">
+                    <span><?=$arResult["IN_NAME"]?></span>
+                </div>
+            <?else:?>
+                <div class="login-information-photo">
+                    <img src="<?=$arResult["PERSONAL_PHOTO"]?>">
+                </div>
+            <?endif?>
+
             <div class="login-information-text widget_user_profile_name">
-                <a href="#"><?=$arResult["LAST_NAME"]?> <?=$arResult["IN_NAMES"]?></a>
+                <a href="#" class="widget_user_profile_name__title">
+                    <?if(!empty($arResult['ACTIVE_COMPANY'])):?><?=$arResult['ACTIVE_COMPANY']['NAME']?>,   <?endif?> <?=$arResult["LAST_NAME"]?> <?=$arResult["IN_NAMES"]?>
+                </a>
                 <?/*<span class="widget_user_profile_url_profile">Профиль</span>*/?>
-                <?if(!empty($arResult['ACTIVE_COMPANY'])):?>
-                    <span class="widget_user_profile_url_profile">Компания: <?=$arResult['ACTIVE_COMPANY']['NAME']?></span>
-                <?endif?>
+
                 <div class="widget_user_profile_select">
                     <ul>
                         <li><a href="/profile/">Редактировать профиль</a></li>
@@ -31,28 +34,5 @@
             </div>
         </div>
     </div>
-    <?}else {?>
-    <? // если нет фото ?>
-    <div class="widget_user_profile_avatar">
-        <span><?=$arResult["IN_NAME"]?></span>
-    </div>
-    <div class="widget_user_profile_name">
-        <span class="widget_user_profile_fio"><?=$arResult["LAST_NAME"]?> <?=$arResult["IN_NAMES"]?></span>
-        <span class="widget_user_profile_url_profile">Профиль</span>
-        <div class="widget_user_profile_select">
-            <ul>
-                <li><a href="/profile/">Редактировать профиль</a></li>
-                <li><a href="/profile/select_company/">Выбрать компанию</a></li>
-                <li><a href="<?echo $APPLICATION->GetCurPageParam("logout=yes", 
-                array(
-                    "login",
-                    "logout",
-                    "register",
-                    "forgot_password",
-                    "change_password"));?>">Выйти</a></li>
-            </ul>
-        </div>
-    </div>
-    <?}?>
 </div>
 
