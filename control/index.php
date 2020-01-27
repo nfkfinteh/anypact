75983db2f6dfa4e0a9b2f2281ed73d4f163b31bf
Solovyov Igor <skiminok2005@gmail.com>
2020-01-27 14:51:32 +0300

    Панель управления пользователями зарегистроованными по акции, компонент и страница

---
+++ control/index.php
@@ -0,0 +1,16 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("AnyPact");
+// проверяем авторизован ли пользователь
+global $USER;
+
+if ($USER->IsAuthorized() && $USER->IsAdmin()){
+?>
+<h3>Панель управления пользователями</h3>
+    <? $APPLICATION->IncludeComponent(
+        "nfksber:control.reguser",
+        "",
+        Array()
+    ); ?>
+<?}?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/components/nfksber/control.reguser/class.php
@@ -0,0 +1,41 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+use Bitrix\Highloadblock as HL;
+use Bitrix\Main\Entity;
+
+class ControlRegUser extends CBitrixComponent
+{
+    public function onPrepareComponentParams($arParams)
+    {
+        $result = array(
+            "CACHE_TYPE"            => $arParams["CACHE_TYPE"],
+            "CACHE_TIME"            => isset($arParams["CACHE_TIME"]) ?$arParams["CACHE_TIME"]: 36000000,
+            "X"                     => intval($arParams["X"]),
+            "IBLOCK_ID"             => intval($arParams["IBLOCK_ID"]),
+            "SECTION_ID"            => intval($arParams["SECTION_ID"]),
+            "ELEMENT_ID"            => intval($arParams["ELEMENT_ID"]),
+            "IBLOCK_ID_CONTRACT"    => intval($arParams["IBLOCK_ID_CONTRACT"]),
+            "TYPE_USER_PROF"        => intval($arParams["TYPE_USER_PROF"])
+        );
+        return $result;
+    }
+
+    function paramsUser($arParams){        
+        $arResult["INFOBLOCK_ID"]       = $arParams["IBLOCK_ID"];
+        $arResult["INFOBLOCK_C_ID"]     = $arParams["IBLOCK_ID_CONTRACT"];
+        $arResult["SECTION_ID"]         = $arParams["SECTION_ID"];
+        $arResult["ELEMENT_ID"]         = $arParams["ELEMENT_ID"];
+        
+        return $arResult;
+    }
+    
+    public function executeComponent()
+    {
+        //$this->arResult["SIGN_DOGOVOR"] = ; 
+        $this->includeComponentTemplate();
+        
+        return $this->arResult;
+    }
+
+}
+?>
