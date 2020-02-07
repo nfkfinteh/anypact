379bfa8316d305d40b520be7336ee7a158eac039
Solovyov Igor <skiminok2005@gmail.com>
2020-02-07 14:10:25 +0300

    fix ссылка в виджете профиля на собщения

--- local/components/nfksber/profile.widget/templates/head/template.php
+++ local/components/nfksber/profile.widget/templates/head/template.php
@@ -1,3 +1,7 @@
+<?
+/*Ссылки на ресурсы*/
+$MessURL = '/list_message/';
+?>
 <div class="widget_user_profile">
     <div class="login-information">
         <div class="login-information-container">
@@ -15,15 +19,19 @@
                 <?endif?>
             <?else:?>
                 <? if(empty($arResult["PERSONAL_PHOTO"])):?>
-                    <div class="widget_user_profile_avatar container_prof">
-                        <span><?=$arResult["IN_NAME"]?></span>
-                        <div class="count_unread <?if($arResult['UNREAD_MESSAGE']>0):?>active<?endif?>"><?=$arResult['UNREAD_MESSAGE']?></div>
-                    </div>
+                    <a href="<?=$MessURL?>">
+                        <div class="widget_user_profile_avatar container_prof">
+                            <span><?=$arResult["IN_NAME"]?></span>
+                            <div class="count_unread <?if($arResult['UNREAD_MESSAGE']>0):?>active<?endif?>"><?=$arResult['UNREAD_MESSAGE']?></div>
+                        </div>
+                    </a>
                 <?else:?>
-                    <div class="login-information-photo container_prof">
-                        <img src="<?=$arResult["PERSONAL_PHOTO"]?>">
-                        <div class="count_unread <?if($arResult['UNREAD_MESSAGE']>0):?>active<?endif?>"><?=$arResult['UNREAD_MESSAGE']?></div>
-                    </div>
+                    <a href="<?=$MessURL?>">
+                        <div class="login-information-photo container_prof">
+                            <img src="<?=$arResult["PERSONAL_PHOTO"]?>">
+                            <div class="count_unread <?if($arResult['UNREAD_MESSAGE']>0):?>active<?endif?>"><?=$arResult['UNREAD_MESSAGE']?></div>
+                        </div>
+                    </a>
                 <?endif?>
             <?endif?>
 
