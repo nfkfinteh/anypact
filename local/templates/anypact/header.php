5e21833abaa9bdd68f91476a71370234c25f0453
Solovyov Igor <skiminok2005@gmail.com>
2020-02-12 10:38:33 +0300

    fix виджет профиля и кнопки добавления объявления замена местами

--- local/templates/anypact/header.php
+++ local/templates/anypact/header.php
@@ -136,15 +136,7 @@ global $USER;
 
                     </div>
                     <div class="col-md-4">
-                        <div class="row">
-                            <div class="col-md-3">
-                                <!--Кнопка создать новое предложение-->
-                                <div class="create-pact-btn">
-                                    <a href="/my_pacts/edit_my_pact/?ACTION=ADD"></a>
-                                    <div>Создать предложение</div>
-                                </div>
-                                <!------------>                   
-                            </div>
+                        <div class="row">                            
                             <div class="col-md-9">
                                 <?                                
                                     $APPLICATION->IncludeComponent("nfksber:profile.widget",
@@ -154,6 +146,14 @@ global $USER;
                                         )
                                     );
                                 ?>
+                            </div>
+                            <div class="col-md-3">
+                                <!--Кнопка создать новое предложение-->
+                                <div class="create-pact-btn">
+                                    <a href="/my_pacts/edit_my_pact/?ACTION=ADD"></a>
+                                    <div>Создать предложение</div>
+                                </div>
+                                <!------------>                   
                             </div>
                         </div>
                     </div>
