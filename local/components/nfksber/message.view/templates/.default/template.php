3eb1f264702b872118c52ba79522298fcd980e34
Solovyov Igor <skiminok2005@gmail.com>
2020-02-14 16:31:29 +0300

    Закрытие переписки, только для участников

--- local/components/nfksber/message.view/templates/.default/template.php
+++ local/components/nfksber/message.view/templates/.default/template.php
@@ -1,108 +1,118 @@
 <?
     $arMessage  =  json_decode($arResult['MESSAGES']['UF_TEXT_MESSAGE_USER'], true);
     $UserID = CUser::GetID();
+    // массив пользователей
+    $arrIDUserChat = array();
+    foreach($arResult['UsersChart'] as $Item){
+        $arrIDUserChat[] = $Item["ID"];
+    }
 ?>
 <pre>
-<? print_r($arResult['UsersChart']);?>
+<? print_r($arrIDUserChat);?>
 </pre>
-<input hidden value="<?=$UserID?>" id="IDUSER" />
-    <div>
-        <h1 class="mb-4">Мои сообщения</h1>
-        <div class="row pt-2 pb-5">
-            <div class="col-md-4 col-sm-12">
-                <h3 class="font-weight-bold">Участники</h3>
-                <ul class="list-person-conversation">
-                    <? foreach($arResult['UsersChart'] as $user){ ?>
-                        <li class="person-conversation">
-                            <div class="person-conversation-photo">                                
-                                <?if ($user['PERSONAL_PHOTO'] !=''){?>
-                                    <? $renderImage = CFile::ResizeImageGet($user['PERSONAL_PHOTO'], Array("width" => 261, "height" => 261), BX_RESIZE_IMAGE_EXACT, false); ?>                               
-                                    <img src="<?=$renderImage['src']?>">
-                                <?}else {?>
-                                    <span class="user-first-letter" style="padding:13px;font-size: 28px;"><?=substr($user['NAME'], 0, 1);?></span>
-                                <?}?>
-                                <?/*<img src="<?=SITE_TEMPLATE_PATH?>/image/sample_face_150x150.png" alt="Васильев Александр Евгеньевич">*/?>
-                            </div>
-                            <div class="person-conversation-name"><?=$user['LAST_NAME']?> <?=$user['NAME']?> <?=$user['SECOND_NAME']?></div>
-                        </li>
-                    <?}?>
-                </ul>
-                <?/*<button class="btn btn-nfk btn-add-person w-100">+ добавить участника</button>*/?>
-                <button class="btn btn-nfk btn-add-person w-100 js-chat_delete">Удалить переписку</button>
-            </div>
-            <div class="col-md-8 col-sm-12">
-                <h3 class="font-weight-bold"><?=$arResult['MESSAGES']['UF_TITLE_MESSAGE']?></h3>
-                <div class="message-list">
-                    <?if ($_REQUEST["ACTION"] == 'up_message') $GLOBALS["APPLICATION"]->RestartBuffer();?>
-                    <? foreach($arMessage as $Message){?>
-                        <? // сообщения текущего пользователя ?>
-                        <?if($Message['user'] == $UserID){?>
-                            <div class="message-block message-block-left">
-                                <div class="message-person-photo">
-                                    <div class="user-avatar">                                                                              
-                                        <? if($arResult['UsersChart'][$Message['user']]['PERSONAL_PHOTO'] != ''){ ?>
-                                            <?                                             
-                                                $arFile = CFile::GetFileArray($arResult['UsersChart'][$Message['user']]['PERSONAL_PHOTO']);
-                                                $renderImage = CFile::ResizeImageGet($arFile, Array("width" => 261, "height" => 261), BX_RESIZE_IMAGE_EXACT, false);                                            
-                                            ?>
-                                            <img src="<?=$renderImage["src"]?>" alt=""/>
-                                        <?}else {?>
-                                            <span class="user-first-letter"><?=$arResult['FastUserParams'][$Message['user']]['InitialName']?></span> 
-                                        <? }?>
-                                    </div>
+<? // если пользователь входит в масив то открываем ему сообщения?>
+<?if(in_array($UserID, $arrIDUserChat)){?>
+    <input hidden value="<?=$UserID?>" id="IDUSER" />
+        <div>
+            <h1 class="mb-4">Мои сообщения</h1>
+            <div class="row pt-2 pb-5">
+                <div class="col-md-4 col-sm-12">
+                    <h3 class="font-weight-bold">Участники</h3>
+                    <ul class="list-person-conversation">
+                        <? foreach($arResult['UsersChart'] as $user){ ?>
+                            <li class="person-conversation">
+                                <div class="person-conversation-photo">                                
+                                    <?if ($user['PERSONAL_PHOTO'] !=''){?>
+                                        <? $renderImage = CFile::ResizeImageGet($user['PERSONAL_PHOTO'], Array("width" => 261, "height" => 261), BX_RESIZE_IMAGE_EXACT, false); ?>                               
+                                        <img src="<?=$renderImage['src']?>">
+                                    <?}else {?>
+                                        <span class="user-first-letter" style="padding:13px;font-size: 28px;"><?=substr($user['NAME'], 0, 1);?></span>
+                                    <?}?>
+                                    <?/*<img src="<?=SITE_TEMPLATE_PATH?>/image/sample_face_150x150.png" alt="Васильев Александр Евгеньевич">*/?>
                                 </div>
-                                <div class="message-container">
-                                    <div class="message-message">
-                                        <p class="user-name-right"><?=$arResult['FastUserParams'][$Message['user']]['FIO']?></p>
-                                        <div class="message-content">
-                                            <div class="message-text">                                                
-                                                <p><?=$Message['message']?></p>
-                                                <time datetime="2019-03-01T15:12:13+03:00"><?=$Message['data']?></time>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>                        
-                        <?}else {?>                            
-                            <div class="message-block message-block-right">                        
-                                <div class="message-person-photo">
-                                    <div class="user-avatar">                                        
-                                        <? if($arResult['UsersChart'][$Message['user']]['PERSONAL_PHOTO'] != ''){ ?>
-                                            <?                                             
-                                                $arFile = CFile::GetFileArray($arResult['UsersChart'][$Message['user']]['PERSONAL_PHOTO']);
-                                                $renderImage = CFile::ResizeImageGet($arFile, Array("width" => 261, "height" => 261), BX_RESIZE_IMAGE_EXACT, false);                                            
-                                            ?>
-                                            <img src="<?=$renderImage["src"]?>" alt="">                                            
-                                        <?}else {?>
-                                            <span class="user-first-letter"><?=$arResult['FastUserParams'][$Message['user']]['InitialName']?></span>
-                                        <? } ?>
-                                    </div>
-                                </div>
-                                <div class="message-container">
-                                    <div class="message-message">
-                                        <p class="user-name-left"><?=$arResult['FastUserParams'][$Message['user']]['FIO']?></p>
-                                        <div class="message-content">
-                                            <div class="message-text">                                                
-                                                <p><?=$Message['message']?></p>
-                                                <time datetime="2019-03-01T15:12:13+03:00"><?=$Message['data']?></time>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                                <div class="person-conversation-name"><?=$user['LAST_NAME']?> <?=$user['NAME']?> <?=$user['SECOND_NAME']?></div>
+                            </li>
                         <?}?>
-                    
-                    <?}?>
-                    <?if ($_REQUEST["ACTION"] == 'up_message') die();?>
-                    <!------>
+                    </ul>
+                    <?/*<button class="btn btn-nfk btn-add-person w-100">+ добавить участника</button>*/?>
+                    <button class="btn btn-nfk btn-add-person w-100 js-chat_delete">Удалить переписку</button>
                 </div>
-                <div class="message-chat-input">
-                    <!-- <button class="mr-1 mr-sm-3"><img src="<?=SITE_TEMPLATE_PATH?>/image/icon-image-file.png" alt=""></button>
-                    <button class="mr-2 mr-sm-4"><img src="<?=SITE_TEMPLATE_PATH?>/image/icon-file.png" alt=""></button> -->
-                    <textarea name="" id="textMessage" placeholder="Введите сообщение"></textarea>
-                    <button class="ml-1 mr-0 mx-sm-4" id="sendMessage"><img src="<?=SITE_TEMPLATE_PATH?>/image/icon-sent.png" alt=""></button>
+                <div class="col-md-8 col-sm-12">
+                    <h3 class="font-weight-bold"><?=$arResult['MESSAGES']['UF_TITLE_MESSAGE']?></h3>
+                    <div class="message-list">
+                        <?if ($_REQUEST["ACTION"] == 'up_message') $GLOBALS["APPLICATION"]->RestartBuffer();?>
+                        <? foreach($arMessage as $Message){?>
+                            <? // сообщения текущего пользователя ?>
+                            <?if($Message['user'] == $UserID){?>
+                                <div class="message-block message-block-left">
+                                    <div class="message-person-photo">
+                                        <div class="user-avatar">                                                                              
+                                            <? if($arResult['UsersChart'][$Message['user']]['PERSONAL_PHOTO'] != ''){ ?>
+                                                <?                                             
+                                                    $arFile = CFile::GetFileArray($arResult['UsersChart'][$Message['user']]['PERSONAL_PHOTO']);
+                                                    $renderImage = CFile::ResizeImageGet($arFile, Array("width" => 261, "height" => 261), BX_RESIZE_IMAGE_EXACT, false);                                            
+                                                ?>
+                                                <img src="<?=$renderImage["src"]?>" alt=""/>
+                                            <?}else {?>
+                                                <span class="user-first-letter"><?=$arResult['FastUserParams'][$Message['user']]['InitialName']?></span> 
+                                            <? }?>
+                                        </div>
+                                    </div>
+                                    <div class="message-container">
+                                        <div class="message-message">
+                                            <p class="user-name-right"><?=$arResult['FastUserParams'][$Message['user']]['FIO']?></p>
+                                            <div class="message-content">
+                                                <div class="message-text">                                                
+                                                    <p><?=$Message['message']?></p>
+                                                    <time datetime="2019-03-01T15:12:13+03:00"><?=$Message['data']?></time>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>                        
+                            <?}else {?>                            
+                                <div class="message-block message-block-right">                        
+                                    <div class="message-person-photo">
+                                        <div class="user-avatar">                                        
+                                            <? if($arResult['UsersChart'][$Message['user']]['PERSONAL_PHOTO'] != ''){ ?>
+                                                <?                                             
+                                                    $arFile = CFile::GetFileArray($arResult['UsersChart'][$Message['user']]['PERSONAL_PHOTO']);
+                                                    $renderImage = CFile::ResizeImageGet($arFile, Array("width" => 261, "height" => 261), BX_RESIZE_IMAGE_EXACT, false);                                            
+                                                ?>
+                                                <img src="<?=$renderImage["src"]?>" alt="">                                            
+                                            <?}else {?>
+                                                <span class="user-first-letter"><?=$arResult['FastUserParams'][$Message['user']]['InitialName']?></span>
+                                            <? } ?>
+                                        </div>
+                                    </div>
+                                    <div class="message-container">
+                                        <div class="message-message">
+                                            <p class="user-name-left"><?=$arResult['FastUserParams'][$Message['user']]['FIO']?></p>
+                                            <div class="message-content">
+                                                <div class="message-text">                                                
+                                                    <p><?=$Message['message']?></p>
+                                                    <time datetime="2019-03-01T15:12:13+03:00"><?=$Message['data']?></time>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?}?>
+                        
+                        <?}?>
+                        <?if ($_REQUEST["ACTION"] == 'up_message') die();?>
+                        <!------>
+                    </div>
+                    <div class="message-chat-input">
+                        <!-- <button class="mr-1 mr-sm-3"><img src="<?=SITE_TEMPLATE_PATH?>/image/icon-image-file.png" alt=""></button>
+                        <button class="mr-2 mr-sm-4"><img src="<?=SITE_TEMPLATE_PATH?>/image/icon-file.png" alt=""></button> -->
+                        <textarea name="" id="textMessage" placeholder="Введите сообщение"></textarea>
+                        <button class="ml-1 mr-0 mx-sm-4" id="sendMessage"><img src="<?=SITE_TEMPLATE_PATH?>/image/icon-sent.png" alt=""></button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+<?}else {?>
+<p>Вы ошиблись чатом</p>
+<?}?>
