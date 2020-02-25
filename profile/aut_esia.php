96438831feabb82e183b89dcedc39d482fa75409
Skiminok <skiminok2005@gmail.com>
2020-02-26 00:28:18 +0300

    fix  проблеммы с подписанием отредактированного договора

--- profile/aut_esia.php
+++ profile/aut_esia.php
@@ -18,33 +18,33 @@ echo "<br> пришли с этого адреса ".$URL_REF;
 //$URL_REF = 'https://anypact.ru/my_pacts/';
 
 $UserTest = 1;
-if ($USER->IsAuthorized()){
-if ($UserTest == 1){
-    session_start();
+// if ($USER->IsAuthorized()){
+// if ($UserTest == 1){
+//     session_start();
 
-    //require_once $_SERVER['DOCUMENT_ROOT'] . '/esia/EsiaLogger.class.php';
-    //EsiaLogger::DumpEnviroment( 'open_gu' );
+//     //require_once $_SERVER['DOCUMENT_ROOT'] . '/esia/EsiaLogger.class.php';
+//     //EsiaLogger::DumpEnviroment( 'open_gu' );
    
-    $_SESSION['id_esia']="";
-    unset($_SESSION['id_esia']);
+//     $_SESSION['id_esia']="";
+//     unset($_SESSION['id_esia']);
 
-    $urlEsia = $_SERVER['DOCUMENT_ROOT']."/esia";
-    include $urlEsia."/Esia.php";
-    include $urlEsia."/EsiaOmniAuth_t.php";
+//     $urlEsia = $_SERVER['DOCUMENT_ROOT']."/esia";
+//     include $urlEsia."/Esia.php";
+//     include $urlEsia."/EsiaOmniAuth_t.php";
 
-    $keys_dir = $_SERVER['DOCUMENT_ROOT'] . '/esia/sert';    
-    $config = array(
-        "site"          => "https://esia.gosuslugi.ru/", //esia portal
-        "redirect_uri"  => $URL_REF,  //callback url
-        "pkey_path"     => $keys_dir."/secret_NFKS01211.key",
-        "cert_path"     => $keys_dir."/cert_NFKS01211.crt",
-        "client_id"     => "NFKS01211",
-        "scope"         => "openid fullname id_doc"
-    );
+//     $keys_dir = $_SERVER['DOCUMENT_ROOT'] . '/esia/sert';    
+//     $config = array(
+//         "site"          => "https://esia.gosuslugi.ru/", //esia portal
+//         "redirect_uri"  => $URL_REF,  //callback url
+//         "pkey_path"     => $keys_dir."/secret_NFKS01211.key",
+//         "cert_path"     => $keys_dir."/cert_NFKS01211.crt",
+//         "client_id"     => "NFKS01211",
+//         "scope"         => "openid fullname id_doc"
+//     );
     
-    $esia = new EsiaOmniAuth($config);
-    $esia->create(); 
-} else {
-    echo "Извините вы не авторизованы.";
-}
+//     $esia = new EsiaOmniAuth($config);
+//     $esia->create(); 
+// } else {
+//     echo "Извините вы не авторизованы.";
+// }
 ?>
