54ffe38054b493f7216c0c134054df3c17b4bcb4
Grimm1994 <Onwardtothegoal@gmail.com>
2020-03-25 17:07:09 +0300

    Заголовок поиска и стили для выпадающего меню профиль

--- local/components/nfksber/profile.widget/templates/head/template_.php
+++ local/components/nfksber/profile.widget/templates/head/template_.php
@@ -11,17 +11,19 @@
             <div class="login-information-text" id="widget_user_profile_name">
                 <a href="#"><?=$arResult["LAST_NAME"]?> <?=$arResult["IN_NAMES"]?></a>
                 <span id="widget_user_profile_url_profile">Профиль</span>
-                <div id="widget_user_profile_select">
+                <div class="forarrow">
+                    <div id="widget_user_profile_select">
                     <ul>
                         <li><a href="/profile/">Редактировать профиль</a></li>
-                        <li><a href="<?echo $APPLICATION->GetCurPageParam("logout=yes", 
+                        <li><a href="<?echo $APPLICATION->GetCurPageParam("logout=yes",
                         array(
-                            "login",
-                            "logout",
-                            "register",
-                            "forgot_password",
-                            "change_password"));?>">Выйти</a></li>
+                        "login",
+                        "logout",
+                        "register",
+                        "forgot_password",
+                        "change_password"));?>">Выйти</a></li>
                     </ul>
+                    </div>
                 </div>
             </div>
         </div>
