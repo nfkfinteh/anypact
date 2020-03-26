53ef465a1def54ef3cb1d3199d4e631c1b69331f
Grimm1994 <Onwardtothegoal@gmail.com>
2020-03-26 11:30:14 +0300

    скрипт открытия и закрытия меню профиля и стили

--- local/components/nfksber/profile.widget/templates/head/template.php
+++ local/components/nfksber/profile.widget/templates/head/template.php
@@ -61,7 +61,7 @@ $MessURL = '/list_message/';
 <script>
     //клик по профилю в шапке
     $(".widget_user_profile_name__title").click(function() {
-        return $(".widget_user_profile_select").slidetoggle("fast") !1
+        return $(".widget_user_profile_select").slideToggle("fast") !1
     })
     // $(document).mouseup(function (e) {
     //     var popup = $('.widget_user_profile_name__title');
