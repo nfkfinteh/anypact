130f5a0ff4688e99520ef4a17b71714159770ef5
Grimm1994 <onwardtothegoal@gmail.com>
2020-04-14 14:49:56 +0300

    фикс закрытия попап

--- local/templates/anypact/js/popup_script.js
+++ local/templates/anypact/js/popup_script.js
@@ -333,7 +333,7 @@ window.onload = function() {
                 if($result['TYPE']=='ERROR'){
                     document.getElementById('message_error_aut').innerHTML = '&#8226; '+$result['VALUE'];
                 }
-                else($result['TYPE']=='SUCCES'){
+                if($result['TYPE']=='SUCCES'){
                     location.reload();
                 }
             });
