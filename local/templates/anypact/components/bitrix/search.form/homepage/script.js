faa2e2585d9c48070625a6ece1de38a7c6c27d94
Grimm1994 <onwardtothegoal@gmail.com>
2020-04-16 13:48:02 +0300

    переворот стрелки в выпадающем меню

--- local/templates/anypact/components/bitrix/search.form/homepage/script.js
+++ local/templates/anypact/components/bitrix/search.form/homepage/script.js
@@ -1,23 +1,5 @@
 $(document).ready(function() { 
-    //открытие меню профиля в шапке
-    $('.widget_user_profile_name__title').on('click', function(){
-        let select = $('.widget_user_profile_select');
-        let visual = $(select).css('display');        
-        if(visual == 'none'){
-            $(select).css('display', 'block');
-            $('.dropdown-arrow-profile').addClass('active-arrow');
-        }else {
-            $(select).css('display', 'none');
-            $('.dropdown-arrow-profile').removeClass('active-arrow');
-        }
-    });
-    $(document).mouseup(function (e) {
-        var popup = $('.widget_user_profile_name__title');
-        if (e.target!=popup[0]&&popup.has(e.target).length === 0){
-            $('.widget_user_profile_select').css('display', 'none');
-            $('.dropdown-arrow-profile').removeClass('active-arrow');
-        }
-    });
+    
     // выбор категории
     $('#button_select_category').on('click', function(){
         let select = $('#select_category_main');
