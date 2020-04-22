bb0c122ca1b8915b0cdef1acb35388625244c972
Трофимов Александр <trofimov.aleksandr@4px.ru>
2020-04-22 16:52:22 +0300

    feat(nfksber.yamap) Загрузка меток по области видмиости карты

--- local/components/nfksber/yamap/templates/.default/template.php
+++ local/components/nfksber/yamap/templates/.default/template.php
@@ -1,6 +1,3 @@
-<div style="display:none;">
-<? //print_r($arResult['ITEMS']);?>
-</div>
 <div class="map-space">
     <!--YM-->
     <script src="https://api-maps.yandex.ru/2.1/?apikey=08f051a6-35f1-4392-a988-5024961ee1a8&lang=ru_RU" type="text/javascript">
@@ -8,14 +5,13 @@
         <div id="map" style="width: <?=$arResult['MAP_WIDTH']?>; height: <?=$arResult['MAP_HEIGHT']?>">
         </div>
         <script type="text/javascript">
-            var mapData = <?=CUtil::PhpToJSObject($arResult['MAP_DATA'])?>;
+            var iblock = <?=CUtil::PhpToJSObject($arResult['IBLOCK_ID'])?>;
             var city = "<?=$arParams['LOCATION']?>";
             if(!city) city = 'Москва';
             // Функция ymaps.ready() будет вызвана, когда
             // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
             ymaps.ready(init);
             function init(){
-
                 ymaps.geocode(city, {
                     results: 1
                 }).then(function (res) {
@@ -26,27 +22,39 @@
                     var myMap = new ymaps.Map("map", {
                         center: coords,
                         zoom: 11,
-                        controls: ['zoomControl'],
-                        width: 100,
-                        height: 100
+                        controls: ['zoomControl']
                     });
-                    myMap.behaviors.disable('scrollZoom');
 
-                    for(var data in mapData){
+                    urlParams = new URLSearchParams(window.location.search);
+                    params = {};
 
-                        myPlacemark = new ymaps.Placemark(mapData[data].geometry.coordinates, {
-                            balloonContent: mapData[data].properties.balloonContent,
-                        }, {
-                            iconLayout: 'default#imageWithContent',
-                            iconImageHref: '<?=SITE_TEMPLATE_PATH//$this->__folder?>/img/map_icon.png',
-                            iconImageSize: [30, 30],
-                            iconImageOffset: [-15, -15],
-                            iconContentOffset: [30, 30],
+                    urlParams.forEach((p, key) => {
+                        params[key] = p;
+                    });
+
+                    var loadingObjectManager = new ymaps.LoadingObjectManager('/response/ajax/map.php'+'?bbox=%b&iblock='+iblock+'&parent='+params.PARENT_SECTION,
+                        {
+                            clusterize: false,
+                            clusterHasBalloon: false,
+                            geoObjectOpenBalloonOnClick: true,
+                            geoObjectIconLayout: 'default#imageWithContent',
+                            geoObjectIconImageHref: '<?=SITE_TEMPLATE_PATH//$this->__folder?>/img/map_icon.png',
+                            geoObjectIconImageSize: [30, 30],
+                            geoObjectIconImageOffset: [-15, -15],
+                            geoObjectIconContentOffset: [30, 30],
+                            //geoObjectIconContentLayout: MyIconContentLayout,
+                            //geoObjectBalloonContentBody:
                         });
 
-                        //Добавляем метки
-                        myMap.geoObjects.add(myPlacemark);
-                    }
+                    myMap.geoObjects.add(loadingObjectManager);
+
+                    myMap.controls.remove('geolocationControl');
+                    myMap.controls.remove('searchControl');
+                    myMap.controls.remove('trafficControl');
+                    myMap.controls.remove('typeSelector');
+                    myMap.controls.remove('fullscreenControl');
+                    myMap.controls.remove('rulerControl');
+                    myMap.behaviors.disable(['scrollZoom']);
                 });
             }
         </script>
---
+++ response/ajax/map.php
@@ -0,0 +1,87 @@
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+if(!CModule::IncludeModule('iblock')){
+    echo json_encode(['VALUE'=>'', 'TYPE'=>'ERROR']);
+    die();
+}
+
+$bbox = $_REQUEST['bbox'];
+$callback = $_REQUEST['callback'];
+$iblockID = $_REQUEST['iblock'];
+$service = $_REQUEST['parent'];
+
+function getCoordinate($str){
+    $ar = explode(',', $str);
+    $arCordinate['x1'] = $ar[0];
+    $arCordinate['y1'] = $ar[1];
+    $arCordinate['x3'] = $ar[2];
+    $arCordinate['y3'] = $ar[3];
+    /*$arCordinate['x2'] = $arCordinate['x1'];
+    $arCordinate['y2'] = $arCordinate['y3'];
+    $arCordinate['x4'] = $arCordinate['x3'];
+    $arCordinate['y4'] = $arCordinate['y1'];*/
+    return $arCordinate;
+}
+
+$arCordinate = getCoordinate($bbox);
+
+
+$arSelect = [
+    'IBLOCK_ID',
+    'ID',
+    'NAME',
+];
+$arFilter = [
+    'IBLOCK_ID'=>$iblockID,
+    'ACTIVE'=>'Y',
+    //'!COORDINATES_AD'=>false,
+    array(
+        "LOGIC" => "AND",
+        array(">=PROPERTY_LONG" => $arCordinate['x1'], "<=PROPERTY_LONG" => $arCordinate['x3']),
+        array(">=PROPERTY_LAT" => $arCordinate['y1'], "<=PROPERTY_LAT" => $arCordinate['y3']),
+    ),
+];
+
+$result = [
+    "type"=>"FeatureCollection",
+    "features"=>[]
+];
+
+$arItems = [];
+
+$items = CIBlockElement::GetList(['SORT'=>'ASC'], $arFilter, false, false, $arSelect);
+
+$i=0;
+while($obj = $items->GetNextElement()){
+    $arItems[$i]['FIELDS'] = $obj->GetFields();
+    $arItems[$i]['PROPERTY']['COORDINATES_AD'] = $obj->GetProperty('COORDINATES_AD');
+    $i++;
+}
+
+$cnt = 0;
+if($arItems){
+    foreach($arItems as $value) {
+        $coordinates = explode(',', $value['PROPERTY']['COORDINATES_AD']['VALUE']);
+        $result["features"][] = array(
+            "type" => "Feature",
+            "id" => $value['FIELDS']['ID'],
+            "geometry" => array(
+                "type" => "Point",
+                "coordinates" => array(
+                    "0" => (double) $coordinates[0],
+                    "1" => (double) $coordinates[1]
+                )
+            ),
+            "properties" => array(
+                //'balloonContent' => '<div class="baloon-content"><a href="'.$value['FIELDS']['DETAIL_PAGE_URL'].'">'.$value['FIELDS']['NAME'].'</a></div>',
+                'balloonContent' => '<div class="baloon-content"><a href="/pacts/view_pact/?ELEMENT_ID='.$value['FIELDS']['ID'].'">'.$value['FIELDS']['NAME'].'</a></div>',
+
+            )
+        );
+
+        $cnt++;
+    }
+}
+
+$jsData = json_encode($result);
+$jsData = $callback.'('.$jsData.')';
+echo $jsData;
