711178ac032b1d978b0d942d0e8e92b528e441d6
Solovyov Igor <skiminok2005@gmail.com>
2020-05-12 16:30:22 +0300

    test esia

--- esia/aut_esia.php
+++ esia/aut_esia.php
@@ -1,28 +1,27 @@
 <?php 
 session_start();
 
-//require_once $_SERVER['DOCUMENT_ROOT'] . '/esia/EsiaLogger.class.php';
-//EsiaLogger::DumpEnviroment( 'open_gu' );
-
 $_SESSION['id_esia']="";
+ 
 unset($_SESSION['id_esia']);
 
-include "Esia.php";
-include "EsiaOmniAuth_t.php";
-$keys_dir = $_SERVER['DOCUMENT_ROOT'] . '/esia/sert';
+$urlEsia = $_SERVER['DOCUMENT_ROOT']."/esia_test";
 
+include $urlEsia."/Esia.php";
+include $urlEsia."/EsiaOmniAuth.php";
+ 
+echo $urlEsia."/EsiaOmniAuth.php";
+
+$keys_dir = $urlEsia.'/sert';  
  
 $config = array(
-"site" => "https://esia.gosuslugi.ru/", //esia portal
-"redirect_uri" => "http://anypact.nfksber.ru/profile/",  //callback url
-//"redirect_uri" => "http://nfksber.game-server.xyz:789/esia/work1.php",  //callback url
-"pkey_path"  => $keys_dir."/secret_NFKS01211.key",
-"cert_path"  => $keys_dir."/cert_NFKS01211.crt",
-"client_id" => "NFKS01211",
-"scope" => "openid fullname id_doc"
- );
+    "site" => "https://esia.gosuslugi.ru/", //esia portal
+    "redirect_uri" => "http://anypact.nfksber.ru/profile/",  //callback url
+    "pkey_path"  => $keys_dir."/secret.key",
+    "cert_path"  => $keys_dir."/cert.crt",    
+    "client_id" => "04VS01",    
+    "scope" => "openid fullname id_doc"
+);
 
 $esia = new EsiaOmniAuth($config);
-$esia->create(); 
-echo "=================";
-?>
+$esia->create();
