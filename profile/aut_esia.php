6bf8761265c8c2cded448272a96a40062bd3c2fe
root <solovyow_iv@nfksber.ru>
2020-05-13 12:53:59 +0300

    fix params esia autorisation

--- profile/aut_esia.php
+++ profile/aut_esia.php
@@ -10,7 +10,6 @@ print_r($_GET);
 $domane = 'https://anypact.ru';
 $ReturnURL = base64_decode($_GET['returnurl']);
 $ReturnURL = $domane.$ReturnURL;
-echo $ReturnURL;
 
 // если пользователь пришел с редактирования контракта нужно ID записи добавить в GET
 if(!empty($_GET['ID_SENDITEM'])){
@@ -19,8 +18,7 @@ if(!empty($_GET['ID_SENDITEM'])){
     $URL_REF = $ReturnURL;
 }
 
-echo "<br> пришли с этого адреса ".$URL_REF;
-//$URL_REF = 'https://anypact.ru/my_pacts/';
+echo "вернуться на ".$URL_REF;
 
 $UserTest = 1;
 //if ($USER->IsAuthorized()){
@@ -35,37 +33,21 @@ if ( $UserTest == 1 ) {
 
     $urlEsia = $_SERVER['DOCUMENT_ROOT']."/esia_test";
 
-    echo "путь до ".$urlEsia;
-    
     include $urlEsia."/Esia.php";
     include $urlEsia."/EsiaOmniAuth.php";
     
     $keys_dir = $urlEsia . '/sert';    
     $config = array(
         "site" => "https://esia.gosuslugi.ru/", //esia portal
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        "redirect_uri" => "http://anypact.nfksber.ru/profile/",  //callback url
-=======
-        "redirect_uri" => "https://anypact.ru/profile/",  //callback url
->>>>>>> b198de1e188ed3e6c903dcf530de588b352009f8
-=======
-        "redirect_uri" => "http://anypact.ru/profile/",  //callback url
->>>>>>> be49c0fe4b0954317354b74510fb2459109f317a
-=======
-        "redirect_uri" => "https://anypact.ru/profile/",  //callback url
->>>>>>> e7ad65d63696f4d0d43f0a8268a7fea697a564ff
+        "redirect_uri" => $URL_REF,  //callback url
         "pkey_path"  => $keys_dir."/secret.key",
         "cert_path"  => $keys_dir."/cert.crt",        
         "client_id" => "04VS01",    
         "scope" => "openid fullname id_doc"
     );
-    echo "<pre>";
-    print_r($config);
-    echo "</pre>";
-    // $esia = new EsiaOmniAuth($config);
-    // $esia->create(); 
+
+    $esia = new EsiaOmniAuth($config);
+    $esia->create(); 
     
 } else {
     echo "Извините вы не авторизованы.";
