88d26caab631da5d5f83566f263d54f32d864698
ras-pl <65278513+ras-pl@users.noreply.github.com>
2020-05-29 14:27:50 +0300

    fix custom mail

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -149,9 +149,9 @@ function custom_mail($to, $subject, $message, $additional_headers, $additional_p
 	$mail->setLanguage('ru');
     $mail->Host   = 'post.nfksber.ru';  // Адрес SMTP сервера
     $mail->SMTPAuth   = true;          // Enable SMTP authentication
-    $mail->Username   = 'info@anypact.ru';       // ваше имя пользователя (без домена и @)
+    $mail->Username   = 'info';       // ваше имя пользователя (без домена и @)
     $mail->Password   = 'PKmR5g3k42';    // ваш пароль
-    $mail->SMTPSecure = 'ssl';         // шифрование ssl
+    $mail->SMTPSecure = 'tls';         // шифрование ssl
     $mail->Port   = 587;
     
     $mail->From = 'info@anypact.ru';
