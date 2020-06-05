3dd7c5d2d9db24aa201e009cf378bb565dfdf4e9
Grimm1994 <Onwardtothegoal@gmail.com>
2020-06-05 16:13:37 +0300

    Update template.php

--- local/components/nfksber/user.list/templates/.default/template.php
+++ local/components/nfksber/user.list/templates/.default/template.php
@@ -32,7 +32,7 @@
                                     <div class="check-esia-img-info">
                                         <span>Подтвержденная авторизация</span>
                                         <img src="<?=SITE_TEMPLATE_PATH?>/img/check-esia-min.png" alt="">
-                                        Госуслуги
+                                        <img src="https://gu-st.ru/st/img/logo_nobeta.0a1f5dfe6b.svg" style="width:50%;"/>
                                     </div>
                                 </div>
                         <?endif?>
@@ -48,7 +48,7 @@
                         <?endif?>
                         <?if($user['UF_ESIA_AUT']):?>
                             <span class="d-block font-weight-bold mt-4">Подтвержденная регистрация</span>
-                            <span class="d-block registration-checked mt-2">Госуслуги</span>
+                            <span class="d-block registration-checked mt-2"><img src="https://gu-st.ru/st/img/logo_nobeta.0a1f5dfe6b.svg" style="width:50%;"/></span>
                         <?else:?>
                             <span class="d-block font-weight-bold mt-4">Регистрация не подтверждена</span>
                         <?endif?>
