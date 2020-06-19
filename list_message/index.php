61559eeecf543026a4cfc32685702ab5e4124cbc
ras-pl <65278513+ras-pl@users.noreply.github.com>
2020-06-19 17:39:38 +0300

    add security

--- list_message/index.php
+++ list_message/index.php
@@ -2,6 +2,11 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("AnyPact || сообщения пользователя");?>
 <?
+global $USER;
+
+if ($USER->IsAuthorized()){
+?>
+<?
 $APPLICATION->IncludeComponent("nfksber:message.list",
     "",
     Array(
@@ -12,5 +17,9 @@ $APPLICATION->IncludeComponent("nfksber:message.list",
 );
 ?>
 </div>
-
+<?
+} else {
+    // заглушка на авторизацию доступа
+    $APPLICATION->IncludeFile(SITE_TEMPLATE_PATH."/not_access.php", Array());
+}?>
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");?>
