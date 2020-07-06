ff712e60ea1f6323dfe259d7c7dd5914234eb1c2
ras-pl <65278513+ras-pl@users.noreply.github.com>
2020-07-06 09:59:30 +0300

    hot fix

--- local/components/nfksber/pactview.detail/templates/.default/template.php
+++ local/components/nfksber/pactview.detail/templates/.default/template.php
@@ -56,7 +56,7 @@ else{
             <?if($arResult['ELEMENT']['ACTIVE']=='Y' && $DATE_ACTIVE_TO>=time()):?>
                 <div class="position-relative not_auth-error-block">
                     <a href="/pacts/view_pact/view_dogovor/?ELEMENT_ID=<?=$arResult["ELEMENT"]["ID"]?>" class="btn btn-nfk cardPact-bBtn <?=$disable_a?>" onclick="ym(64629523,'reachGoal','docs_link');">Посмотреть или подписать договор</a>
-                    <?if(!empty($disable_a)):?>
+                    <?if($arResult['USER']['UF_ESIA_AUT'] != 1):?>
                         <div class="not_auth-error">
                             <span class="triangle" style="display: block; z-index: 1;">▲</span>
                             <div>Для подписания предложения необходимо <a target="__blank" href="/profile/#aut_esia">подтвердить свой аккаунт с помощью учетной записи портала Госуслуг</a></div>
