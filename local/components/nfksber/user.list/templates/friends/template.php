ec7a33ccf7454aad18317650d629c4a1f7943d6e
ras-pl <65278513+ras-pl@users.noreply.github.com>
2020-08-05 17:49:37 +0300

    hot fix

--- local/components/nfksber/user.list/templates/friends/template.php
+++ local/components/nfksber/user.list/templates/friends/template.php
@@ -174,7 +174,7 @@
                 break;
         }
     ?>
-    <p>Список <?=$text?> пуст.</p><?if($arParams['FRIENDS_STATUS'] != "B" && $arParams['FRIENDS_STATUS'] != "S"){?><p>Воспользуйтейтесь <a href="/search_people/">поиском</a>.</p><?}?>
+    <p>Список <?=$text?> пуст.</p><?if($arParams['FRIENDS_STATUS'] == "Y" && $arParams['FRIENDS_STATUS'] == "O"){?><p>Воспользуйтейтесь <a href="/search_people/">поиском</a>.</p><?}?>
 <?}?>
 </div>
 
