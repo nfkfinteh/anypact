dd4afeed21547dbb09b1dcf20593098fe98c0851
ras-pl <65278513+ras-pl@users.noreply.github.com>
2020-08-18 14:27:31 +0300

    fix comment

--- local/components/nfksber/comment.list/templates/.default/template.php
+++ local/components/nfksber/comment.list/templates/.default/template.php
@@ -8,7 +8,11 @@ IncludeTemplateLangFile(__FILE__);
         <?foreach ($arResult['ITEMS'] as $item):?>
             <div class="row align-items-center mt-4 comment-block">
                 <div class="col-3 col-sm-2  col-lg-1">
-                    <img src="<?=$item['USER']['PERSONAL_PHOTO']?>" class="cardPact-comment-avatar" alt="">
+                    <?
+                    $resizeImg = "/upload/resize_image/".$item['USER']['ID']."/resize_image_65px.img";
+                    $res = CFile::ResizeImageFile($item['USER']['PERSONAL_PHOTO'], $resizeImg, array("width" => "65", "height" => "65"), BX_RESIZE_IMAGE_EXACT);
+                    ?>
+                    <img src="<?=$resizeImg?>" class="cardPact-comment-avatar" alt="">
                 </div>
                 <div class="col-9 col-sm-10 col-lg-7">
                     <div class="cardPact-comment-header">
@@ -36,13 +40,13 @@ IncludeTemplateLangFile(__FILE__);
         <?endforeach?>
         <?if(!$arResult['BLACKLIST']){?>
             <?if($_POST['COMMENT_EDIT']=='Y'):?>
-                <div class="offset-sm-2 offset-lg-1 col-sm-10 col-lg-7" id="edit_comment_container">
+                <div class="col-sm-10 col-lg-8" id="edit_comment_container">
                     <textarea placeholder="Оставьте Ваш комментарий"  maxlength="500"><?=trim($arResult['EDIT_COMMENT']['UF_TEXT_MESSAGE'])?></textarea>
                     <input type='hidden' name='ACTION' value='add' />
                     <button class="btn btn-nfk cardPact-comment-submit" data-coment_id="<?=$arResult['EDIT_COMMENT']['ID']?>">Сохранить</button>
                 </div>
             <?else:?>
-                <div class="offset-sm-2 offset-lg-1 col-sm-10 col-lg-7" id="comment_container">
+                <div class="col-sm-10 col-lg-8" id="comment_container">
                     <textarea placeholder="Оставьте Ваш комментарий"  maxlength="500"></textarea>
                     <input type='hidden' name='ACTION' value='add' />
                     <button class="btn btn-nfk cardPact-comment-submit">Комментировать</button>
