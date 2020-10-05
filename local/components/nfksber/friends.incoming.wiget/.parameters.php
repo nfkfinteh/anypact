85b453d0588cecf42d4a8ed396355c2ed8b23aa0
ras-pl <65278513+ras-pl@users.noreply.github.com>
2020-10-05 09:33:49 +0300

    fix merge

--- local/components/nfksber/friends.incoming.wiget/.parameters.php
+++ local/components/nfksber/friends.incoming.wiget/.parameters.php
@@ -1,46 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
 if(!CModule::IncludeModule("iblock"))
-	return;
-
-"VARIABLE_ALIASES" => array( 
-      "list" => array(),
-      "section" => array(
-                        "IBLOCK_ID" => "BID",
-                        "SECTION_ID" => "ID"
-                        ),
-      "element" => array(
-      "SECTION_ID" => "SID",
-      "ELEMENT_ID" => "ID"
-      ),
-)
-
-=======
-=======
->>>>>>> d9fc60356232591f7133ff592e5fe98c9f412c53
-<?
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
-if(!CModule::IncludeModule("iblock"))
-	return;
-
-"VARIABLE_ALIASES" => array( 
-      "list" => array(),
-      "section" => array(
-                        "IBLOCK_ID" => "BID",
-                        "SECTION_ID" => "ID"
-                        ),
-      "element" => array(
-      "SECTION_ID" => "SID",
-      "ELEMENT_ID" => "ID"
-      ),
-)
-
-<<<<<<< HEAD
->>>>>>> d9fc60356232591f7133ff592e5fe98c9f412c53
-=======
->>>>>>> d9fc60356232591f7133ff592e5fe98c9f412c53
+      return;
 ?>
