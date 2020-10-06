ba6aaf539829aa62bd797a94a534f9728be1b5c3
ras-pl <65278513+ras-pl@users.noreply.github.com>
2020-10-06 11:29:43 +0300

    50r page

---
+++ promotion/.section.php
@@ -0,0 +1,6 @@
+<?
+$sSectionName = "AnyPact || 50 рублей на телефон за 5 минут!";
+$arDirProperties = Array(
+
+);
+?>
---
+++ promotion/index.php
@@ -0,0 +1,77 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("AnyPact || 50 рублей на телефон за 5 минут!");
+global $USER;
+if($USER -> IsAdmin()/* || !$USER -> IsAuthorized()*/){
+    ?>
+    </div>
+    <section class="section-50r">
+        <div class="container-akcii">
+            <div class="row-akcii">
+                <div class="col-50r">
+                    <h1 class="h1-50r">
+                        <span class="h1-big">50 рублей</span>
+                        <span class="h1-small">на телефон за 5 минут!</span>
+                        Хочешь получить?!
+                    </h1>
+                </div>
+                <div class="col-50r">
+                    <img src="<?=SITE_TEMPLATE_PATH?>/image/iphone-50r.png" alt="">
+                </div>
+            </div>
+            <div class="regbox-akcii">
+                <span class="text-desc">Зарегистрируйся на сайте AnyPact.ru и получи деньги<br> на мобильный телефон.*</span>
+                <button class="btn btn-nfk reg-btn-akcii" id="open_reg_form">Регистрация</button>
+            </div>
+            <h2>Для этого необходимо<br> пройти четыре обязательных шага!</h2>
+            <div class="row-steps">
+                <div class="steps_item">
+                    <span>1</span>
+                    <div class="desc-box">
+                        <h3>Регистрация</h3>
+                        <p>Регистрируемся на сайте <a href="https://anypact.ru/">AnyPact.ru</a>;</p>
+                    </div>
+                </div>
+                <div class="steps_item">
+                    <span>2</span>
+                    <div class="desc-box">
+                        <h3>Заполнение</h3>
+                        <p>Заполняем свой профиль в настройках (обязательно с вашим фото и актуальным номером телефона - на него и произойдет зачисление средств);</p>
+                    </div>
+                </div>
+                <div class="steps_item">
+                    <span>3</span>
+                    <div class="desc-box">
+                        <h3>Подтверждение</h3>
+                        <p>Подтверждаем свои данные через портал ГОСУСЛУГИ;</p>
+                    </div>
+                </div>
+                <div class="steps_item">
+                    <span>4</span>
+                    <div class="desc-box">
+                        <h3>Размещение</h3>
+                        <p>Размещаем объявление с фотографией о реальном намерении продать или купить товар/работу/услугу.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row-end">
+                <img src="<?=SITE_TEMPLATE_PATH?>/image/coin-bg.png" alt=""/>
+                <h2>
+                    <span class="h1-big">
+                        Всё!
+                    </span>
+                    <span class="h1-small">
+                        Как только объявление будет проверено и опубликовано, баланс телефона будет пополнен.
+                    </span>
+                </h2>
+                <p>
+                    AnyPact.ru – это новая и очень удобная площадка, позволяющая совершать дистанционные сделки купли/продажи различных товаров, работ или услуг, а также совершать другие операции с использованием простой идентификации участников сделок через ГОСУСЛУГИ.
+                </p>
+                <span class="text-desc">*Предложение распространяется на первую тысячу зарегистрировавшихся пользователей, при условии, что ранее Вы не регистрировались на сайте AnyPact.ru. Предложение заканчивает свое действие 31.10.2020.</span>
+            </div>
+        </div>
+    </section>
+<?}else{
+    header("Location: /");
+}?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
