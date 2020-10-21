f9801dada4a45a24c4c828599012a21b59043c94
root <solovyow_iv@nfksber.ru>
2020-10-21 17:39:31 +0300

    fix 1

---
+++ rules/index.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("AnyPact || Бесплатный сервис для дистанционного заключения сделок");
+?>
+<div class="rules-block">
+    <iframe src="/upload/rules/user_rules.pdf"/></iframe>
+</div>
+</div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
