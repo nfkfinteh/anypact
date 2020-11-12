737b0afa1b97a669da1ee64e14cb60789cc161b5
ras-pl <65278513+ras-pl@users.noreply.github.com>
2020-11-12 16:12:12 +0300

    add whitelist to user fields update

--- response/ajax/edit_personal.php
+++ response/ajax/edit_personal.php
@@ -28,6 +28,43 @@ if(!empty($data) || !empty($imgData)){
         unset($fields['CONFIRM_PASSWORD']);
     }
 
+    $arWhiteList = [
+        "LAST_NAME",
+        "NAME",
+        "SECOND_NAME",
+        "PERSONAL_GENDER",
+        "PERSONAL_BIRTHDAY",
+        "UF_SNILS",
+        "UF_INN",
+        "EMAIL",
+        "PASSWORD",
+        "CONFIRM_PASSWORD",
+        "PERSONAL_ZIP",
+        "PERSONAL_CITY",
+        "PERSONAL_COUNTRY",
+        "UF_STREET",
+        "PERSONAL_STATE",
+        "UF_N_HOUSE",
+        "UF_N_HOUSING",
+        "UF_N_APARTMENT",
+        "UF_REGION",
+        "PERSONAL_PHONE",
+        "UF_WORK",
+        "UF_EDUCATION",
+        "UF_ABOUT",
+        "UF_HIDE_PROFILE",
+        "UF_DISPLAY_PHONE",
+        "UF_DISPLAY_DATE",
+        "UF_DISPLAY_ADDRESS",
+        "UF_N_BANK",
+        "UF_KS_BANK",
+        "UF_BIC_BANK",
+        "UF_RS_BANK",
+        "UF_INN_BANK",
+        "UF_OTHER_PARAMS_BANK",
+        "PERSONAL_PHOTO"
+    ];
+
     $arDelFields = [
         "LOGIN",
         "UF_SPASSPORT",
@@ -58,6 +95,10 @@ if(!empty($data) || !empty($imgData)){
     if(!empty($fields['NAME'])) $fields['NAME'] = mb_convert_case($fields['NAME'], MB_CASE_TITLE);
     if(!empty($fields['LAST_NAME'])) $fields['LAST_NAME'] = mb_convert_case($fields['LAST_NAME'], MB_CASE_TITLE);
     if(!empty($fields['SECOND_NAME'])) $fields['SECOND_NAME'] = mb_convert_case($fields['SECOND_NAME'], MB_CASE_TITLE);
+
+    foreach($fields as $key => $value)
+        if(!in_array($key, $arWhiteList))
+            unset($fields[$key]);
 
     $user = new CUser;
     $satus = $user->Update($idUser, $fields);
