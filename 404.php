de0cc98c81e88e9791c291f81cac7823a2e82a17
ras-pl <65278513+ras-pl@users.noreply.github.com>
2020-12-01 17:26:11 +0300

    fix 404

--- 404.php
+++ 404.php
@@ -1,10 +1,11 @@
 <?
-include_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/urlrewrite.php');
+$doc_root = "/var/www/domains/anypact.nfksber.ru";
+include_once($doc_root.'/bitrix/modules/main/include/urlrewrite.php');
 
 CHTTP::SetStatus("404 Not Found");
 @define("ERROR_404","Y");
 
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+require($doc_root."/bitrix/header.php");
 
 $APPLICATION->SetTitle("404 Not Found");
 
@@ -24,4 +25,4 @@ $APPLICATION->SetTitle("404 Not Found");
             изменено или временно недоступно. <a href="/">Вернуться на главную страницу</a></p>
     </div>
 </div>
-<? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php"); ?>
+<? require($doc_root."/bitrix/footer.php"); ?>
