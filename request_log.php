a173ffa6085021c380d32e2939dfb222db99640f
root <solovyow_iv@nfksber.ru>
2021-03-09 15:53:28 +0300

    новые иконки в футер

---
+++ request_log.php
@@ -0,0 +1,12 @@
+<?
+function writeToLog($data, $title = '') {
+    $log = "\n------------------------\n";
+    $log .= date("Y.m.d G:i:s") . "\n";
+    $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
+    $log .= print_r($data, 1);
+    $log .= "\n------------------------\n";
+    file_put_contents(getcwd() . '/34rtesdtgf56yrtg.log', $log, FILE_APPEND);
+    return true;
+}
+writeToLog($_REQUEST, 'REQUEST');
+?>
