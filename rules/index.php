3e0b3e35686e2a336e0ef3e953bfc7a9dc94ccbf
root <solovyow_iv@nfksber.ru>
2021-03-15 12:54:21 +0300

    поменял ссылку на /rules/

--- rules/index.php
+++ rules/index.php
@@ -3,7 +3,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("AnyPact || Условия использования сайта");
 ?>
 <div class="rules-block">
-    <iframe src="/upload/rules/user_rules.pdf"/></iframe>
+    <iframe src="/upload/rules/user_rules_7.pdf"></iframe>
 </div>
 </div>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
