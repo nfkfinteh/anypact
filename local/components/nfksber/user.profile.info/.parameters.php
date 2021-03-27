013b72b4498e25725688b8bad7a0a6ecabc7302e
root <solovyow_iv@nfksber.ru>
2021-03-27 14:30:17 +0300

    fix

--- local/components/nfksber/user.profile.info/.parameters.php
+++ local/components/nfksber/user.profile.info/.parameters.php
@@ -4,16 +4,5 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 if(!CModule::IncludeModule("iblock"))
 	return;
 
-"VARIABLE_ALIASES" => array( 
-      "list" => array(),
-      "section" => array(
-                        "IBLOCK_ID" => "BID",
-                        "SECTION_ID" => "ID"
-                        ),
-      "element" => array(
-      "SECTION_ID" => "SID",
-      "ELEMENT_ID" => "ID"
-      ),
-)
 
 ?>
