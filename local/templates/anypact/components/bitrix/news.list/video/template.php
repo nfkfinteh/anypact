22fc170e0f000c982226c1348b0c220e093d36d4
root <solovyow_iv@nfksber.ru>
2021-05-19 10:13:15 +0300

    видео о сервисе

--- local/templates/anypact/components/bitrix/news.list/video/template.php
+++ local/templates/anypact/components/bitrix/news.list/video/template.php
@@ -1,9 +1,11 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<? if(!empty($arResult["ITEMS"])): ?>
-    <? foreach($arResult["ITEMS"] as $arItem): ?>
-        <? if ($arItem["DISPLAY_PROPERTIES"]["VIDEO"]["FILE_VALUE"]): ?>
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<? if (!empty($arResult["ITEMS"])) : ?>
+    <? foreach ($arResult["ITEMS"] as $arItem) : ?>
+        <? if ($arItem["DISPLAY_PROPERTIES"]["VIDEO"]["FILE_VALUE"]) : ?>
             <div class="video_block">
-                <video src="<?= $arItem["DISPLAY_PROPERTIES"]["VIDEO"]["FILE_VALUE"]["SRC"] ?>" controls poster="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"></video>
+                <video controls poster="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>">
+                    <source src="<?= $arItem["DISPLAY_PROPERTIES"]["VIDEO"]["FILE_VALUE"]["SRC"] ?>" src="video/bt-1400.mp4" type='video/mp4' />
+                </video>
             </div>
         <? endif ?>
     <? endforeach ?>
